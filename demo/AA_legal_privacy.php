95e65045dbf75b07d3c8a11c0347b5dfe7ca09d9
remy <remy@stripthis.com>
2016-02-04 10:51:34 +0530

    0.3.13

--- demo/AA_legal_privacy.php
+++ demo/AA_legal_privacy.php
@@ -47,38 +47,136 @@
 			<div class="grid grid-responsive-12">
 				<div class="row">
 					<div class="col7">
-						<div class="plugin-check-wrapper">
-							<h2>Privacy Policy</h2>
-						</div>
-						<div>
-						<p>
-							Lorem ipsum dolor sit amet, mel ea salutatus torquatos gloriatur. Error ubique est an. Ex nam ludus maiorum moderatius, mel cu diceret complectitur mediocritatem, ne meis omnes vix. Te inani doctus ocurreret has, agam officiis et vis. Usu eu congue latine, vim interesset consequuntur an.
-						</p>
-						<p>
-							Eos nibh solum definitiones eu, ne his diam quaestio. Eos te erant ornatus, mea at erat choro. Usu ad dico antiopam, id per vocent persius sententiae. Eu impedit scriptorem usu, ex pro ridens abhorreant, affert propriae adversarium vis ei.
-						</p>
-						</div>
-						<div>
-							<h3>Definitions</h3>
-							<p>
-								Sed mutat tamquam recteque te, sed ei unum luptatum. Sea ea admodum maluisset conceptam, nam no bonorum disputando, pri vidit docendi scaevola ut. Dicunt partiendo ei per, vitae volutpat at ius. Ei iriure delenit epicuri has, cu diam corrumpit est. Ea eam fabellas oportere philosophia, est ei justo scriptorem, eripuit maiorum dignissim ne mea. Ei quo modo dicit commune, at eam harum vocibus intellegebat.
-							</p>
-							<h4>Cookies</h4>
-							<p>
-								Id saepe melius usu. Eu mei viris eruditi nominati. Percipit periculis pri an, unum sententiae neglegentur an vel. Eos ex dicat omittam ullamcorper, ea ceteros tibique invidunt pro. Simul expetenda mediocritatem eum ea.
-							</p>
-							<h4>Analytics</h4>
-							<p>
-								Id vis utinam torquatos dissentiunt, saperet mentitum ea duo, nulla phaedrum maluisset nam at. Errem oblique eu qui, ea conceptam definitionem per. Erant tation liberavisse ut duo, ad qui argumentum dissentias, et quo falli aliquam. Graeco splendide vix ea. Fastidii electram pri ei, id integre pericula consulatu sed. Te nam dicta labitur democritum.
-							</p>
-						</div>
-
+						<article>
+							<div>
+								<h2>Privacy Policy</h2>
+								<p>
+									This website, passbolt.com is owned and operated by Bolt Softwares Private Limited ("we", "us" or "passbolt"). By using and accessing our Site, you ("you", "user" or, "end user") agree to the terms of our Privacy Policy (collectively, the "privacy policy").
+								</p>
+								<p>
+									This Privacy Policy is effective with respect to any data that we’ve collected, or collect, about and/or from you, according to our <a href="#">Terms of Service</a>.
+								</p>
+							</div>
+							<div>
+								<h3>Definitions</h3>
+								<p>
+									We define ‘Personal Information’ (which refers with no exception to, “your information”) in the following manner:
+								</p>
+								<p>
+									Any information that you provide to us about yourself while using the service that could help someone else  identify you as an individual entity. This may  include information such as your name, phone number, location, IP address, system locale and preferences, picture, public key information, etc.
+								</p>
+								<p>
+									We define this website (also referred to as the "Site", or “Service”) as <a href="http://www.passbolt.com">www.passbolt.com</a> and any other subdomains such as <a href="https://demo.passbolt.com">demo.passbolt.com</a> (also reffered as “Demo Site”).
+								</p>
+							</div>
+							<div>
+								<h3>Cookies and Tracking</h3>
+								<h4>Cookies</h4>
+								<p>
+									A <a href="https://support.mozilla.org/en-US/kb/cookies-information-websites-store-on-your-computer" target="_blank">cookie</a> is information stored on your computer by a website you visit.
+									This Site use cookies for two purposes:
+								</p>
+								<ul>
+									<li>Sessions, e.g. to provide you with the functionality that keeps you logged in or to make sure your preferences are carried forward.</li>
+									<li>For analytics (see next section). We do not use cookies to track you on third party sites.</li>
+								</ul>
+								<h4>Analytics</h4>
+								<p>
+									We use <a href="http://piwik.org/" target="_blank" rel="nofollow">Piwik</a> to collect information about your access to the Site.
+									It is possible for you to opt-out of this analytics tracking by enabling your browser's "<a href="https://en.wikipedia.org/wiki/Do_Not_Track" target="_blank" rel="nofollow">Do Not Track</a>" preference.
+								</p>
+								<p>
+									The Demo Site itself collect information about your access to provide functionality such as access and control logs within the application. It is not possible for your to opt-out.
+								</p>
+								<h4>Social buttons</h4>
+								<p>
+									To protect you from third party tracking, we do not include any third party javascript application such as “facebook like” or “google +1” buttons on this Site.
+								</p>
+							</div>
+							<div>
+								<h3>Our use of your personal information</h3>
+								<p>
+									We may use your personal information only for one or more of the following purposes:
+								</p><p>
+									<strong>To give you access to the Service.</strong> For example, if you register to the demo section of the Site we may send you a link by email to activate your account.
+								</p><p>
+									<strong>To notify you about any activity within the Service.</strong> For example if you are using the demo section of the Site and if another user shares a password with you, we may send you an email notification.
+								</p><p>
+									<strong>To provide you with support.</strong> For example, if you leave your personal information on the help section of the Site, we may contact you to help you solve your issues or answer your questions.
+								</p><p>
+									<strong>To promote our services.</strong> For example, if we think you might benefit from using another service we offer, or if we think an information about a change in the current Service is relevant for you, we may contact you to tell you about it.
+								</p><p>
+									<strong>To bill and collect money owed to us.</strong> This includes communications with regards to invoices, receipts, payment statuses and processing issues.
+								</p>
+							</div>
+							<div>
+								<h3>Disclosure of personal information</h3>
+								<p>
+									We may disclose your Personal Information only for one or more of the following purposes:
+								</p><p>
+									<strong>To provide you with the Service.</strong> For example, if you register to the demo section of the Site other people will be able to see the email address and name you used to register and will be able to share information with you.
+								</p><p>
+									<strong>To meet legal requirements.</strong> In the event we are to comply with court orders and valid subpoenas or to defend a court, arbitration, or similar proceeding.
+								</p><p>
+									<strong>To provide information to representatives and advisors.</strong> These include engineers, attorneys and accountants, who help us comply with legal, accounting, or security requirements. All staff or external advisors are required to sign a nondisclosure agreement prior to getting access to your personal data.
+								</p><p>
+									<strong>To transfer your information</strong> In the case of a sale, merger, consolidation, or acquisition, any acquirer will be subject to our obligations under this privacy policy, including your rights to edit and delete your personal data. We will notify you of the change either by sending you an email or posting a notice on our Web site, so that you can opt-out if you wish to do so.
+								</p>
+							</div>
+							<div>
+								<h3>3rd Party Access</h3>
+								<p>
+									We will not give, sell, rent or loan any personal information to any third party. The following third parties may have access to your personal data under certain conditions.
+								</p>
+								<h4>Governments</h4>
+								<p>
+									Our organization is registered in India. Therefore, we are obliged to comply with the Government of India lawful interception requests pursuant to the Rule 419A of Indian Telegraph Rules.
+								</p><p>
+									Our organization relies on services (such as hosting, help desk and newsletters) provided by companies registered in the USA. They are obliged to provide access to notices pursuant to the Digital Millennium Copyright Act as well as judicial, regulatory or other governmental orders or requests valid in USA.
+								</p>
+								<h4>Hosting provider</h4>
+								<p>
+									We use Amazon Web Services to host our websites.
+									See AWS legal framework here: https://aws.amazon.com/legal/
+								</p><p>
+								<h4>Newsletter provider</h4>
+								<p>
+									We use Mailchimp to send newsletters.
+									See mailchimp legal framework here: http://mailchimp.com/legal/
+								</p>
+								<h4>Help desk provider</h4>
+								<p>
+									We use Zendesk to provide support by email or on social networks.
+									See Zendesk legal framework here: https://www.zendesk.com/company/privacy/
+								</p>
+								<h4>Analytics</h4>
+								<p>
+									We use Piwik Cloud Pro for analytics.
+									See the privacy section in the Piwik terms of services https://cloud.piwik.pro/tos/
+								</p>
+								<h3>Deleting, editing and accessing your personal information</h3>
+								<p>
+									You can stop receiving our promotional emails by following the unsubscribe instructions included in every email.
+								</p><p>
+									You can stop receiving Service notification emails by deleting your account in the demo section our our Site. To delete your account you will need to log in and go to your profile and click on the delete button.
+								</p><p>
+									You may also contact us to edit or delete your personal information by using the contact form. Unless it’s prohibited by law, we’ll remove or edit any of your personal information from our servers at your request.
+								</p><p>
+									We can also give you access to any personal information that we hold within 30 days of any request you make by contacting us: contact@passbolt.com.
+								</p>
+							</div>
+							<div>
+								<h3>Date of Last Update</h3>
+								<p>
+									This privacy policy was last updated on January 26, 2016.
+								</p>
+							</div>
+						</article>
 					</div>
 					<div class="col5 last">
-						<img src="../src/img/illustrations/legal_275.jpg" class="illustration" alt="Three little birds"/>
+						<img src="img/illustrations/legal_275.jpg" class="illustration"/>
 					</div>
 				</div>
-
 			</div>
 		</div>
 	</div>
