4a01613417485f736397aedccac9dadc7772ae2c
remy <remy@stripthis.com>
2016-02-12 22:26:31 +0530

    0.3.20

---
+++ demo/AA_home_prelaunch_thankyou.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html class="no-js passbolt version alpha" lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome | Passbolt</title>
+	<!--
+			 ____                  __          ____
+			/ __ \____  _____ ____/ /_  ____  / / /_
+		   / /_/ / __ `/ ___/ ___/ __ \/ __ \/ / __/
+		  / ____/ /_/ (__  |__  ) /_/ / /_/ / / /_
+		 /_/    \__,_/____/____/_.___/\____/_/\__/
+
+		 Open source password manager for teams
+		 © 2016 Bolt Softwares Pvt Ltd
+
+	 -->
+	<base href="../src/">
+	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+	<meta name="description" content="Open source password manager for teams">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="css/prelaunch.css" />
+
+</head>
+<body>
+<!-- main - html5 -->
+<div id="container" class="page prelaunch">
+
+	<div class="grid-wrapper">
+		<div class="grid">
+			<div class="row">
+				<div class="col5 last">
+					<div class="logo">
+						<h1><span>Passbolt</span></h1>
+					</div>
+					<p>
+						Thanks for your support, see you soon!		</p>
+				</div>
+			</div>
+			<div class="row" >
+				<div class="col5 social thanks last">
+					<a href="https://twitter.com/passbolt" class="twitter">
+						<i class="fa fa-twitter-square"></i>
+						<span class="visuallyhidden">follow us on twitter!<span>
+					</a>
+					<a href="https://www.facebook.com/passbolt" class="facebook">
+						<i class="fa fa-facebook-square"></i>
+						<span class="visuallyhidden">follow us on facebook!<span>
+					</a>
+				</div>
+				<div class="teaser bg-picture">
+					<span class="hidden">Copyright: abstract of bokeh on the road - Nuchy Lee / shutterstock</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- footer -->
+<footer>
+	<div class="footer">
+		<span class="copyright">© 2016 Bolt Softwares Pvt Ltd </span></div>
+</footer>
+<noscript><p><img src="//passbolt.piwikpro.com//piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+</body>
+</html>
