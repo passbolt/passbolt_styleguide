69ea55a6afa5313a241350b2a3e2ca27ab945ce9
Cedric Alfonsi <cedricalfonsi@gmail.com>
2016-02-16 14:50:03 +0100

    PASSBOLT-1278 hide session expire dialog close button with css. version 0.3.23

--- demo/includes/dialogs/LU_session_expired.php
+++ demo/includes/dialogs/LU_session_expired.php
@@ -8,6 +8,10 @@
     <div class="dialog">
         <div class="dialog-header">
             <h2>Session expired</h2>
+            <a class="dialog-close js-dialog-close" href="#">
+                <i class="fa fa-close"></i>
+                <span class="visuallyhidden">close</span>
+            </a>
         </div>
         <div class="js_dialog_content dialog-content">
             <div class="form-content">
