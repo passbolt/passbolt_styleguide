6bc1c1618b4dc5281e9d4a64f4679ac42cac99dd
Cedric Alfonsi <cedricalfonsi@gmail.com>
2016-03-09 11:56:01 +0100

    PASSBOLT-1246 Remove html relative to the create/edit password in the sharing mockups. Resolve previous merge.

--- demo/includes/dialogs/LU_password_share_autocomplete.php
+++ demo/includes/dialogs/LU_password_share_autocomplete.php
@@ -26,51 +26,9 @@
                 </ul>
 
                 <div class="js_tabs_content tabs-content">
-                    <form style="display: none;" class="passbolt_form_resource_create tab-content mad_view_form hidden" id="js_rs_edit"
-                        action="../demo/LU_passwords.php"><div class="form-content">
-
-                            <input value="" name="passbolt.model.Resource.Category.id" id="js_field_category_id" class="form_field mad_form_textbox form-element mad_view_form_textbox ready" type="hidden">
-
-                            <div class="input text required js_form_element_wrapper">
-                                <label for="js_field_name">Name</label>
-                                <input name="passbolt.model.Resource.name" class="required mad_form_textbox form-element mad_view_form_textbox ready" maxlength="50" id="js_field_name" placeholder="name" type="text">
-                                <div id="js_field_name_feedback" class="message mad_form_feedback js_component mad_view ready">
-                                </div>
-                            </div>
-
-                            <div class="input text js_form_element_wrapper">
-                                <label for="js_field_uri">URI</label>
-                                <input class="mad_form_textbox form-element mad_view_form_textbox ready" name="passbolt.model.Resource.uri" maxlength="50" id="js_field_uri" placeholder="https://example.com/login" type="text">
-                                <div id="js_field_uri_feedback" class="message mad_form_feedback js_component mad_view ready">
-                                </div>
-                            </div>
-
-                            <div class="input text required js_form_element_wrapper">
-                                <label for="js_field_username">Username</label>
-                                <input name="passbolt.model.Resource.username" class="required mad_form_textbox form-element mad_view_form_textbox ready" maxlength="50" id="js_field_username" placeholder="name" type="text">
-                                <div id="js_field_username_feedback" class="message mad_form_feedback js_component mad_view ready">
-                                </div>
-                            </div>
-                            <div class="js_form_element_wrapper js_form_secret_wrapper">
-                                <label for="js_field_secret_data_0">Password</label>
-                                <div id="js_secret_edit_0" class="js_secret_edit_form passbolt_form_secret_create form mad_view_form ready">
-                                    <input value="560a6da4-6814-4fb5-babc-1d48c0a8006f" name="passbolt.model.Secret.id" id="js_field_secret_id_0" class="js_field_secret_id form_field mad_form_textbox form-element mad_view_form_textbox ready" type="hidden">
-                                    <input value="cd49eb9e-73a2-3433-a018-6ed993d421e8" name="passbolt.model.Secret.user_id" id="js_field_secret_user_id_0" class="js_field_secret_user_id form_field mad_form_textbox form-element mad_view_form_textbox ready" type="hidden">
-                                    <textarea name="passbolt.model.Secret.data" id="js_field_secret_data_0" class="form_field hidden js_field_secret_data mad_form_textbox form-element mad_view_form_textbox ready" data-view-id="371"></textarea>
-                                </div>
-                                <iframe src="about:blank?passbolt=decryptInline" id="passbolt-iframe-secret-edition" frameborder="0"></iframe></div>
-                            <div class="input textarea js_form_element_wrapper">
-                                <label for="js_field_description">Description</label>
-                                <textarea name="passbolt.model.Resource.description" class="required mad_form_textbox form-element mad_view_form_textbox ready" maxlength="150" id="js_field_description" placeholder="add a description" data-view-id="372">		</textarea>
-                                <div id="js_field_description_feedback" class="message mad_form_feedback js_component mad_view ready">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="submit-wrapper clearfix">
-                            <input class="button primary" value="save" data-view-id="373" type="submit">
-                            <a href="#" class="js-dialog-cancel cancel">cancel</a>
-                        </div>
-                    </form><div class="passbolt_component_permissions share-tab tab-content passbolt_view_component_permissions ready selected" id="js_rs_permission"><div class="form-content permission-edit">
+                    <form style="display: none;" class="passbolt_form_resource_create tab-content mad_view_form hidden" id="js_rs_edit"></form>
+                    <div class="passbolt_component_permissions share-tab tab-content passbolt_view_component_permissions ready selected" id="js_rs_permission">
+                        <div class="form-content permission-edit">
                             <ul id="js_permissions_list" class="permissions scroll mad_component_tree mad_view_component_tree ready">
                                 <li id="44a51f44-6f9f-3c03-af9b-3e80fd50c1f6" class="row direct-permission" data-view-id="382">
                                     <div class="avatar">
@@ -204,7 +162,8 @@
                                         </a>
 
                                     </div>
-                                </li></ul>
+                                </li>
+                            </ul>
                         </div>
                         <div id="js_permissions_changes" class="warning message hidden">
                             <span>You need to save to apply the changes.</span>
