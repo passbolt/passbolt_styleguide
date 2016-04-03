e817dfc0ba1844289dda9154c7c81d286b155755
remy <remy@stripthis.com>
2016-04-03 17:16:45 +0530

    1.0.12

--- demo/AA_home.php
+++ demo/AA_home.php
@@ -38,16 +38,28 @@
         <div class="swiper-container">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
-                    <img src="../src/img/screenshots/screentest.png" />
+                    <img src="../src/img/screenshots/AN_login.jpg" />
                     <h3>Authenticate using your gpg key</h3>
                 </div>
                 <div class="swiper-slide">
-                    <img src="../src/img/screenshots/screentest.png" />
+                    <img src="../src/img/screenshots/LU_password_share.jpg" />
                     <h3>Share passwords with your team</h3>
                 </div>
                 <div class="swiper-slide">
-                    <img src="../src/img/screenshots/screentest.png" />
+                    <img src="../src/img/screenshots/LU_passwords.jpg" />
                     <h3>Search and filter on passwords</h3>
+                </div>
+                <div class="swiper-slide">
+                    <img src="../src/img/screenshots/LU_users.jpg" />
+                    <h3>Manage your team members</h3>
+                </div>
+                <div class="swiper-slide">
+                    <img src="../src/img/screenshots/LU_password_edit.jpg" />
+                    <h3>Edit and review your password complexity</h3>
+                </div>
+                <div class="swiper-slide">
+                    <img src="../src/img/screenshots/LU_profile_keyring.jpg" />
+                    <h3>Import and export your gpg keys</h3>
                 </div>
             </div>
             <div class="swiper-button-prev"></div>
