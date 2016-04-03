4636058e8889192c3b931d751e256cee80ee179f
remy <remy@stripthis.com>
2016-04-03 16:27:34 +0530

    1.0.10

--- demo/includes/AA_meta.php
+++ demo/includes/AA_meta.php
@@ -12,3 +12,4 @@
 	<base href="../src/">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" type="image/x-icon" href="img/webroot/favicon.ico" />
+	<meta property="og:image:secure_url" content="img/background/og_preview.jpg" />
