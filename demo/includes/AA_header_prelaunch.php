0c3611322d2078a572d8c320776fd3fd7eff479a
remy <remy@stripthis.com>
2016-05-06 20:59:10 +0530

    PASSBOLT-1475 Features, paper icon and fontawesome svg

--- demo/includes/AA_header_prelaunch.php
+++ demo/includes/AA_header_prelaunch.php
@@ -7,6 +7,7 @@
 					<li class="home"><a href="../demo/AA_home.php"><span>home</span></a></li>
 					<li><a href="../demo/AA_home.php"><span>home</span></a></li>
 					<li><a href="../demo/AN_login.php"><span>demo</span></a></li>
+					<li><a href="../demo/AA_roadmap.php"><span>roadmap</span></a></li>
 					<li><a href="../demo/AA_help.php"><span>help</span></a></li>
 					<li><a href="../demo/AA_about_credits.php"><span>about</span></a></li>
 				</ul>
---
+++ demo/includes/AA_signup.php
@@ -0,0 +1,33 @@
+<div class="page-row signup">
+    <div class="grid grid-responsive-12">
+        <div class="row">
+            <div class="col12 last">
+                <h2>Let's keep in touch!</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col8 newsletter">
+                <div class="registration form">
+                    <form action="/" id="RegistrationsPrelaunchForm" method="post" accept-charset="utf-8"><div style="display:none;"><input type="hidden" name="_method" value="POST"/></div>
+                        <div class="inline email input">
+                            <label for="RegistrationEmail">Signup to the newsletter</label>
+                            <input name="data[Registration][email]" class="required fluid" placeholder="Your email" type="email" id="RegistrationEmail" required="required"/></div>
+                        <div class="submit"><input  class="button primary" type="submit" value="subscribe"/></div>
+                        <div class="smallprint"><a href="#">We respect your privacy.</div>
+                    </form>
+                </div>
+            </div>
+            <div class="col4 last social">
+                <a href="https://www.twitter.com/passbolt">
+                    <i class="fa fa-fw fa-twitter-square"></i>
+                </a>
+                <a href="https://www.facebook.com/passbolt">
+                    <i class="fa fa-fw fa-facebook-square"></i>
+                </a>
+                <a href="https://www.github.com/passbolt">
+                    <i class="fa fa-fw fa-github-square"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
