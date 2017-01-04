5ac7686b206d788ba452abf667f1ca4beee18c12
remy <remy@passbolt.com>
2017-01-04 18:44:42 +0530

    1.3.0

---
+++ demo/AA_start.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html class="alpha version no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Passbolt | The open source password manager for teams</title>
+    <?php include('includes/AA_meta.php'); ?>
+	<?php include('includes/AA_header_scripts.php'); ?>
+</head>
+<body>
+<div id="container" class="page start ">
+    <?php include('includes/AA_header.php'); ?>
+
+    <div class="page-row">
+        <div class="grid grid-responsive-12">
+            <div class="row">
+                <article>
+                <div class="col8">
+                    <h1>Welcome to passbolt!</h1>
+                    <p class="message success">Passbolt plugin is successfully installed!</p>
+	                <p class="message warning">The plugin it is not configured for any domain yet.</p>
+
+                    <h2>Where do we go from there?</h2>
+                    <ul>
+                        <li>If you have registered on a passbolt instance, check your mailbox for a confirmation link.</li>
+                        <li>If your passbolt administrator sent you an invitation, also check your mailbox.</li>
+                        <li>If you are just trying it out passbolt, head for our
+                            <a href="https://demo.passbolt.com">demo site</a>.</li>
+                    </ul>
+                </div>
+                <div class="col4 last">
+                    <div class="tldr">
+                        <h2>Do you need help?</h2>
+                        <p>
+                            You are lost and you want to talk to a human. We got your back.
+                        </p>
+                        <a href="mailto:contact@passbolt.com" class="button primary">Contact us!</a>
+                    </div>
+                </div>
+                </article>
+            </div>
+        </div>
+    </div>
+
+    <?php include('includes/AN_footer.php'); ?>
+</div>
+</body>
+</body>
+</html>
---
+++ demo/AN_register_error.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html class="passbolt no-js no-passboltplugin version alpha" lang="en">
+<head>
+	<title>Register</title>
+	<?php include('includes/ALL_meta.php'); ?>
+	<link rel="stylesheet" type="text/css" href="css/login.css" />
+	<link rel="stylesheet" type="text/css" href="css/devel.css" />
+<body>
+<div id="container" class="register page">
+	<?php include('includes/ALL_top_warning_messages.php'); ?>
+	<?php include('includes/AN_header_first.php'); ?>
+
+	<!-- main -->
+	<div class="grid">
+		<div class="row">
+			<div class="col6 push1 information">
+				<h2>Try passbolt today!</h2>
+				<p>
+					 Enter your details in the form.
+					We will send you an email to get you started.
+				</p>
+				<?php include('includes/AN_disclaimer_demo.php');?>
+				<?php include('includes/AN_agreement_notice.php');?>
+			</div>
+			<div class="col4 push1 last">
+
+				<div class="logo">
+					<h1><span>Passbolt</span></h1>
+				</div>
+				<div class="users register form">
+					<form action="AN_register_thankyou.php" id="UserLoginForm" method="post" accept-charset="utf-8">
+						<input type="hidden" name="_method" value="POST"/>
+						<fieldset>
+							<legend>Please enter your username and password</legend>
+							<div class="input text required"><label for="UserFirstname">Firstname</label>
+								<input name="data[User][firstname]" class="required fluid" maxlength="50" type="text" id="UserFirstname" required="required"/></div>
+							<div class="input text required error">
+								<label for="UserLastname">Lastname</label>
+								<input name="data[User][lastname]" class="required fluid" maxlength="50" type="text" id="UserLastname" required="required"/>
+								<div class="error message">Last name should be between 2 and 64 characters long</div>
+							</div>
+							<div class="input text required"><label for="UserUsername">Email</label>
+								<input name="data[User][username]" class="required fluid" maxlength="50" type="text" id="UserUsername" required="required"/></div>
+						</fieldset>
+						<div class="actions-wrapper">
+							<div class="submit"><input  type="submit" class="button primary big" value="register" /></div>
+							<a class="secondary" href="../demo/AN_login.php">already a member?</a>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<?php include('includes/AN_promoblock_github.php'); ?>
+			<?php include('includes/AN_promoblock_chromeplugin.php'); ?>
+			<?php include('includes/AN_promoblock_donate.php'); ?>
+		</div>
+	</div>
+</div>
+<?php include('includes/AN_footer.php'); ?>
+<div id="js-devel-sql-trace" class="devel-sql-trace grid">
+	<div class="row">
+		<div class="col12">
+			<table class="cake-sql-log" id="cakeSqlLog_14329906465569b3b6c4f022_69163107"><caption>(default) 2 queries took 0 ms</caption>	<thead>
+				<tr><th>Nr</th><th>Query</th><th>Error</th><th>Affected</th><th>Num. rows</th><th>Took (ms)</th></tr>
+				</thead>
+				<tbody>
+				<tr><td>1</td><td>SELECT `Role`.`id`, `Role`.`name`, `Role`.`description`, `Role`.`created`, `Role`.`modified`, `Role`.`created_by`, `Role`.`modified_by` FROM `passbolt`.`roles` AS `Role`   WHERE `name` IN (&#039;admin&#039;, &#039;user&#039;)</td><td></td><td style="text-align: right">2</td><td style="text-align: right">2</td><td style="text-align: right">0</td></tr>
+				<tr><td>2</td><td>SELECT `AuthenticationBlacklist`.`id`, `AuthenticationBlacklist`.`ip`, `AuthenticationBlacklist`.`expiry`, `AuthenticationBlacklist`.`created`, `AuthenticationBlacklist`.`modified` FROM `passbolt`.`authentication_blacklists` AS `AuthenticationBlacklist`   WHERE `expiry` &gt; &#039;2015-05-30 12:57:26&#039;</td><td></td><td style="text-align: right">0</td><td style="text-align: right">0</td><td style="text-align: right">0</td></tr>
+				</tbody></table>
+		</div>
+	</div>
+</div>
+</body>
+</html>
