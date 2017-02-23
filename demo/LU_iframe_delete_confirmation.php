e691f8e49f82508882362f1872a5d26a898d4be2
remy <remy@passbolt.com>
2017-02-23 18:16:49 +0100

    PASSBOLT-1928 Delete confirmation dialog

--- demo/LU_iframe_delete_confirmation.php
+++ demo/LU_iframe_delete_confirmation.php
@@ -7,7 +7,7 @@
 <div class="dialog-wrapper">
 	<div class="dialog confirm">
 		<div class="dialog-header">
-			<h2>Do you really want to delete password ?</h2>
+			<h2>Do you really want to delete password?</h2>
 			<a class="dialog-close js-dialog-close" href="#">
 				<i class="fa fa-close"></i>
 				<span class="visuallyhidden">close</span>
@@ -16,7 +16,7 @@
 		<div class="js_dialog_content dialog-content">
 			<div class="form-content">
 				<p>
-					Please confirm you really want to delete the password. Ater clicking ok, the password will be <strong>deleted permanently</strong>.
+					Please confirm you really want to delete the password. After clicking ok, the password will be <strong>deleted permanently</strong>.
 				</p>
 			</div>
 
---
+++ demo/includes/dialogs/LU_user_group_delete_confirmation.php
@@ -0,0 +1,24 @@
+<div class="dialog-wrapper">
+    <div class="dialog confirm">
+        <div class="dialog-header">
+            <h2>Are you sure?</h2>
+            <a class="dialog-close js-dialog-close" href="#">
+                <i class="fa fa-close"></i>
+                <span class="visuallyhidden">close</span>
+            </a>
+        </div>
+        <div class="js_dialog_content dialog-content">
+            <div class="form-content">
+                <p>
+                    <strong>You are about to delete the group "IT Support"!</strong>
+                </p>
+                <p>This group is not associated with any password. You are good to go!</p>
+            </div>
+
+            <div class="submit-wrapper clearfix">
+                <input type="submit" value="delete group" class="button primary warning">
+                <a class="js-dialog-close cancel" href="#">cancel</a>
+            </div>
+        </div>
+    </div>
+</div>
