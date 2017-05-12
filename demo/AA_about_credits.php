2ee02274eb8cde51aca0abfc1b5a952c1c7fe417
remy <remy@passbolt.com>
2017-05-12 14:46:57 +0530

    1.5.3

--- demo/AA_about_credits.php
+++ demo/AA_about_credits.php
@@ -46,26 +46,42 @@
 						<div>
 							<h3>Special thanks</h3>
 							<p>
-								Passbolt wouldn't exist without the support and patience of our early bakers, our friends and loved ones.
-								To Ismael, Myriam, Aurelie, Seema, Diego, Janosh and all our brothers and sisters who gave us a hand,
-								thank you for believing in us.
+								Passbolt could not exist without the support and patience of our families and friends.
+								A special thank you to all of you who gave us a hand and are spreading the word.
 							</p>
 						</div>
 						<div>
-							<h3>Team rocket</h3>
+							<h3>Passbolt Core Team</h3>
 							<ul>
 								<li>
-									Shenanigans: <a href="https://github.com/cedricalfonsi">Cedric Alfonsi</a>
+									<a href="https://github.com/cedricalfonsi">Cedric Alfonsi</a>
 								</li><li>
-									Disorganization: <a href="https://github.com/stripthis">Remy Bertot</a>
+									<a href="https://github.com/stripthis">Remy Bertot</a>
 								</li><li>
-									Troublemaking: <a href="https://github.com/kevinmuller">Kevin Muller</a>
+									<a href="https://github.com/kevinmuller">Kevin Muller</a>
 								</li><li>
-									Doodling: <a href="https://www.behance.net/arthurcduarte">Arthur Duarte</a>
-								</li><li>
-									Spelling mistakes: <a href="https://www.youtube.com/watch?v=RNkEr9ntN1A">Anhad Imaan</a>
-								</li><li>
-									Monkey business: <a href="https://in.linkedin.com/in/shrutimuller">Shruti Metha</a>
+									<a href="https://github.com/dlen">Diego Lendoiro</a>
+								</li>
+								<li>and our wonderful community!</li>
+							</ul>
+						</div>
+						<div>
+							<h3>Icons and illustrations</h3>
+							<ul>
+								<li>
+									"<a href="https://github.com/passbolt/passbolt_styleguide/tree/master/src/img/illustrations">Passbolt illustrations</a>" by
+									Passbolt / <a href="https://www.behance.net/arthurcduarte" target="_blank">Arthur Duarte</a> are licensed under
+									<a href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA-4.0</a>
+								</li>
+								<li>
+									"<a href="https://snwh.org/paper/icons/">Paper Icons</a>" by
+									<a href="https://samuelhewitt.com/" target="_blank">Sam Hewitt</a> is licensed under
+									<a href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA-4.0</a>
+								</li>
+								<li>
+									"<a href="http://fontawesome.io">Font Awesome</a>" by
+									<a href="https://twitter.com/davegandy" target="_blank">Dave Gandy</a> is licensed under
+									<a href="http://creativecommons.org/licenses/by/3.0/">CC-BY-3.0</a>
 								</li>
 							</ul>
 						</div>
@@ -74,70 +90,73 @@
 							<p>And of course a big thank you to the amazing communities of all the following open source projects:</p>
 							<ul class="double">
 								<li>
-									<a href="http://cakephp.org" target="_blank">Cakephp</a>
+									<a href="http://cakephp.org" target="_blank">Cakephp (MIT)</a>
 								</li><li>
-									<a href="http://canjs.com/" target="_blank">CanJS</a>
+									<a href="http://canjs.com/" target="_blank">CanJS (MIT)</a>
 								</li><li>
-									<a href="http://www.javascriptmvc.com/" target="_blank">Javascript MVC</a>
+									<a href="http://www.javascriptmvc.com/" target="_blank">Javascript MVC (MIT)</a>
 								</li><li>
-									<a href="https://developer.mozilla.org/en-US/Add-ons/SDK" target="_blank">Firefox SDK</a>
+									<a href="https://developer.mozilla.org/en-US/Add-ons/SDK" target="_blank">Firefox SDK (MPL-2.0)</a>
 								</li><li>
-									<a href="http://openpgpjs.org/" target="_blank">OpenGPG.js</a>
+									<a href="http://openpgpjs.org/" target="_blank">OpenPGP.js (LGPL)</a>
 								</li><li>
-									<a href="https://www.docker.com/" target="_blank">Docker</a>
+									<a href="https://www.docker.com/" target="_blank">Docker (ASL)</a>
 								</li><li>
-									<a href="http://gruntjs.com/" target="_blank">Grunt</a>
+									<a href="http://gruntjs.com/" target="_blank">Grunt (MIT)</a>
 								</li><li>
-									<a href="https://www.npmjs.com/" target="_blank">Npm</a>
+									<a href="https://www.npmjs.com/" target="_blank">Npm (Artistic License)</a>
 								</li><li>
-									<a href="http://underscorejs.org/" target="_blank">UnderscoreJS</a>
+									<a href="http://underscorejs.org/" target="_blank">UnderscoreJS (MIT)</a>
 								</li><li>
-									<a href="https://mochajs.org/" target="_blank">Mocha</a>
+									<a href="https://mochajs.org/" target="_blank">Mocha (MIT)</a>
 								</li><li>
-									<a href="http://www.apache.org/" target="_blank">Apache</a>
+									<a href="http://www.apache.org/" target="_blank">Apache (ASL)</a>
 								</li><li>
-									<a href="https://gpgauth.org/" target="_blank">gpgAuth</a>
+									<a href="https://gpgauth.org/" target="_blank">gpgAuth (N/A)</a>
 								</li><li>
-									<a href="https://www.openssl.org" target="_blank">OpenSSL</a>
+									<a href="https://www.openssl.org" target="_blank">OpenSSL (ASL)</a>
 								</li>
 							</ul>
 							<ul class="double">
 								<li>
-									<a href="lesscss.org/" target="_blank">Less.js</a>
+									<a href="lesscss.org/" target="_blank">Less.js (ASL)</a>
 								</li><li>
-									<a href="https://fortawesome.github.io/Font-Awesome/" target="_blank">Font awesome</a>
+									<a href="http://www.seleniumhq.org" target="_blank">Selenium HQ (ASL)</a>
 								</li><li>
-									<a href="http://www.seleniumhq.org" target="_blank">Selenium HQ</a>
+									<a href="https://phpunit.de/" target="_blank">Phpunit (BSD)</a>
 								</li><li>
-									<a href="https://phpunit.de/" target="_blank">Phpunit</a>
+									<a href="http://piwik.org/" target="_blank">Piwik (GPL)</a>
 								</li><li>
-									<a href="http://piwik.org/" target="_blank">Piwik</a>
+									<a href="https://git-scm.com/" target="_blank">Git (GPL)</a>
 								</li><li>
-									<a href="https://git-scm.com/" target="_blank">Git</a>
+									<a href="https://www.debian.org/" target="_blank">Debian (DFSG)</a>
 								</li><li>
-									<a href="https://www.debian.org/" target="_blank">Debian</a>
+									<a href="https://jenkins-ci.org/" target="_blank">Jenkins (MIT)</a>
 								</li><li>
-									<a href="https://jenkins-ci.org/" target="_blank">Jenkins</a>
+									<a href="https://jquery.com/" target="_blank">Jquery (MIT)</a>
 								</li><li>
-									<a href="https://jquery.com/" target="_blank">Jquery</a>
+									<a href="http://chaijs.com/" target="_blank">Chai (MIT)</a>
 								</li><li>
-									<a href="http://chaijs.com/" target="_blank">Chai</a>
+									<a href="https://www.owasp.org" target="_blank">OWASP (CC BY-SA)</a>
 								</li><li>
-									<a href="https://www.owasp.org" target="_blank">OWASP</a>
+									<a href="https://www.gnupg.org/" target="_blank">GnuPG (GPL)</a>
 								</li><li>
-									<a href="https://www.gnupg.org/" target="_blank">GnuPG</a>
-								</li><li>
-									And all the others we forgot!
+									Let us know if we forgot anyone!
 								</li>
 							</ul>
 						</div>
 					</div>
 					<div class="col5 last" >
-						<div class="tldr alt photoalbum">
-							<img src="img/photos/IMG_20150207_174133317.jpg" class="photo photo-1" />
-							<img src="img/photos/IMG_20150205_110635987.jpg" class="photo photo-2" />
-							<img src="img/photos/IMG_20150913_112147699.jpg" class="photo photo-3" />
-							<div class="madeinindia">Made with <i class="fa fa-heart"><span class="visuallyhidden">love</span></i> in goa</div>
+						<h1>Sponsors</h1>
+						<div class="tldr alt sponsors">
+							<a href="https://www.luxinnovation.lu/innovate-in-luxembourg/performance-programmes/fit-4-start/">
+								<img src="img/third_party/ministry_of_economy_luxembourg_logo.png" />
+								<img src="img/third_party/luxinnovation_logo.svg"/>
+								<img src="img/third_party/technoport_logo.png" />
+							</a>
+							<a href="https://www.virtua.ch" target="_blank">
+								<img src="img/third_party/virtua_logo.png"/>
+							</a>
 						</div>
 					</div>
 				</div>
