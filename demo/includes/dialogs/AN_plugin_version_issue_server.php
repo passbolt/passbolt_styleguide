138658040fc3a62d087388ef1b9b050cb2fb3422
remy <remy@passbolt.com>
2017-05-18 19:52:25 +0530

    PASSBOLT-2072 As a user using incompatible API/plugin version I should see a warning on the login page

---
+++ demo/includes/dialogs/AN_plugin_version_issue_server.php
@@ -0,0 +1,23 @@
+<div class="dialog-wrapper">
+    <div class="dialog confirm delete-group-dialog">
+        <div class="dialog-header">
+					<h2>Version issue: update the server.</h2>
+        </div>
+        <div class="js_dialog_content dialog-content">
+            <div class="form-content">
+                <p>
+									<strong>The API is not compatible with this plugin version.</strong><br>
+									Your passbolt administrator need to update passbolt backend to use the latest API version.
+								</p>
+								<p>
+									API: v1.4.0 /
+									Plugin: v1.5.0
+								</p>
+            </div>
+            <div class="submit-wrapper clearfix">
+								<a class="button primary" role="link" href="#">Learn more</a>
+                <a class="cancel" role="button" href="../demo/AN_login">or proceed with caution</a>
+            </div>
+        </div>
+    </div>
+</div>
