e5d301024830d85d615404f12772c25681956368
Remy Bertot <remy@passbolt.com>
2017-11-02 14:23:43 +0530

    PASSBOLT-2474 Update roadmap with community forum button

--- demo/AA_roadmap.php
+++ demo/AA_roadmap.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Passbolt | Open source password manager for teams</title>
     <?php include('includes/meta/AA_meta.php'); ?>
-		<?php include('includes/headers/AA_header_scripts.php'); ?>
+    <?php include('includes/headers/AA_header_scripts.php'); ?>
 </head>
 <body>
 <div id="container" class="page roadmap featured">
@@ -15,14 +15,15 @@
                 <div class="col12">
                 <h1>Features & roadmap</h1>
                 <p>
-                    Find out what we are currently working on and what we have in store for the future!
-                    Tell us if you feel something is missing!
+                    Here are all the next majors milestone for passbolt in the near future.
+                    You can vote for new ideas and change requests in the community forum.
                 </p>
+                <a href="https://community.passbolt.com" class="button primary big">Discuss the roadmap!</a>
                 </div>
             </div>
         </div>
     </div>
-		<?php include('includes/public/AA_roadmap1.php'); ?>
+    <?php include('includes/public/AA_roadmap1.php'); ?>
     <?php include('includes/AA_signup.php'); ?>
     <?php include('includes/AN_footer.php'); ?>
 </div>
