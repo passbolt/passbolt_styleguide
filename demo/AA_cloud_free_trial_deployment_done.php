4be328a1441ce6433535ffadd2c94344b989c05a
Kevin Muller <kevin@passbolt.com>
2018-05-22 17:55:35 +0200

    progress bar for free trial

---
+++ demo/AA_cloud_free_trial_deployment_done.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html class="alpha version no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Passbolt | The open source password manager for teams</title>
+    <?php include('includes/meta/AA_meta.php'); ?>
+	<?php include('includes/headers/AA_header_scripts.php'); ?>
+</head>
+<body>
+<div id="container" class="page featured free-trial-deployment-done">
+    <?php include('includes/headers/AA_header.php'); ?>
+
+    <div class="page-row free-trial-deployment-done">
+        <div class="grid grid-responsive-12">
+            <div class="row">
+	            <div class="col12">
+		            <div class="message success">
+			            <h1><i class="fa fa-check-circle"></i> Your free trial is ready</h1>
+		            </div>
+
+					<p>You will now be redirected to your passbolt where you can continue and setup your account</p>
+                    <div class="input-wrapper">
+                        <a href="#" class="button primary next big processing">wait</a>
+                        <a href="#">Click here if you can't wait</a>.
+                    </div>
+	            </div>
+            </div>
+		</div>
+    </div>
+
+    <?php include('includes/AN_footer.php'); ?>
+</div>
+</body>
+</html>
