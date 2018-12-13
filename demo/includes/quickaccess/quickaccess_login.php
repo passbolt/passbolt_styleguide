cbd0c6fe2fa8f75efbd80bf0b9f61ca25833f3d9
Remy Bertot <remy@passbolt.com>
2018-12-13 17:14:32 +0100

    Fix checkbox

--- demo/includes/quickaccess/quickaccess_login.php
+++ demo/includes/quickaccess/quickaccess_login.php
@@ -13,7 +13,7 @@
         </div>
         <div class="input checkbox small">
             <input type="checkbox" name="remember-me" id="remember-me">
-            <label for="rememberMe">Remember until I log out.</label>
+            <label for="remember-me">Remember until I log out.</label>
         </div>
         <div class="submit-wrapper">
             <a id="login-submit" href="#" class="button primary big full-width" role="button">login</a>
