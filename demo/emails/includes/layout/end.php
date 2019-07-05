76f04de19f6d7f1eb3cde3f2e5e522972020ace1
Remy Bertot <remy@passbolt.com>
2019-07-05 17:16:26 +0200

    Extend emails

---
+++ demo/emails/includes/layout/end.php
@@ -0,0 +1,10 @@
+                </table>
+                <!-- // EMAIL CONTAINER -->
+            </td>
+        </tr>
+    </table>
+    <!-- // BODY -->
+    <?php include('footer.php'); ?>
+</center>
+</body>
+</html>
