43d74607f6181c514a2688816b4dcc2341404cf7
Priyanga V <priyanga@passbolt.com>
2019-09-16 13:02:24 +0530

    User create button focus issue fixed
    Signed-off-by: Priyanga V <priyanga@passbolt.com>

--- demo/includes/headers/AA_header.php
+++ demo/includes/headers/AA_header.php
@@ -9,7 +9,7 @@
                     <li><a href="demo/AA_services_pricing.php"><span>Pricing</span></a></li>
 					<li><a href="demo/AA_roadmap.php"><span>Roadmap</span></a></li>
                     <li><a href="http://community.passbolt.com/"><span>Forum</span></a></li>
-					<li><a href="demo/AA_help.php"><span>Help</span></a></li>                   <li><a href="demo/AA_cloud_free_trial.php" class="highlighted"><span>try it!</span></a></li>
+					<li><a href="demo/AA_help.php"><span>Help</span></a></li>
                     <li><a href="demo/AA_cloud_free_trial.php" class="highlighted"><span>try it!</span></a></li>
                 </ul>
 				<button class="hidden">More</button>
--- demo/includes/headers/LU_header_third_main_action_users.php
+++ demo/includes/headers/LU_header_third_main_action_users.php
@@ -1,9 +1,9 @@
 <div class="col1 main-action-wrapper">
 	<div class="dropdown">
-		<a id="user_workspace_create_button" href="#" class="button primary mad_component_button_dropdown js_component mad_view_component_button_dropdown ready">
+		<a id="user_workspace_create_button" href="#" class="button create primary mad_component_button_dropdown js_component mad_view_component_button_dropdown ready">
             <span class="svg-icon">
                 <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1344 960v-128q0-26-19-45t-45-19h-256v-256q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v256h-256q-26 0-45 19t-19 45v128q0 26 19 45t45 19h256v256q0 26 19 45t45 19h128q26 0 45-19t19-45v-256h256q26 0 45-19t19-45zm320-64q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z" fill="#fff"/>
+                    <path d="M1344 960v-128q0-26-19-45t-45-19h-256v-256q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v256h-256q-26 0-45 19t-19 45v128q0 26 19 45t45 19h256v256q0 26 19 45t45 19h128q26 0 45-19t19-45v-256h256q26 0 45-19t19-45zm320-64q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z" />
                 </svg>
             </span>
 			<span>Create</span>
