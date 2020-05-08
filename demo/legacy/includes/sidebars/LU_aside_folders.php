5c09fc1c7fa96d80bfcb3fb279beb1be1d637045
Cedric Alfonsi <cedric@passbolt.com>
2020-05-08 23:35:24 +0200

    Fix sidebar resource details and long resource name
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- demo/legacy/includes/sidebars/LU_aside_folders.php
+++ demo/legacy/includes/sidebars/LU_aside_folders.php
@@ -7,17 +7,18 @@
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"></path></svg>
           </span>
         </div>
-        <h3>
-          <span class="name">Plants
-            <a class="title-link" title="Copy the link to this folder">
-                <span class="svg-icon light">
-                    <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1520 1216q0-40-28-68l-208-208q-28-28-68-28-42 0-72 32 3 3 19 18.5t21.5 21.5 15 19 13 25.5 3.5 27.5q0 40-28 68t-68 28q-15 0-27.5-3.5t-25.5-13-19-15-21.5-21.5-18.5-19q-33 31-33 73 0 40 28 68l206 207q27 27 68 27 40 0 68-26l147-146q28-28 28-67zm-703-705q0-40-28-68l-206-207q-28-28-68-28-39 0-68 27l-147 146q-28 28-28 67 0 40 28 68l208 208q27 27 68 27 42 0 72-31-3-3-19-18.5t-21.5-21.5-15-19-13-25.5-3.5-27.5q0-40 28-68t68-28q15 0 27.5 3.5t25.5 13 19 15 21.5 21.5 18.5 19q33-31 33-73zm895 705q0 120-85 203l-147 146q-83 83-203 83-121 0-204-85l-206-207q-83-83-83-203 0-123 88-209l-88-88q-86 88-208 88-120 0-204-84l-208-208q-84-84-84-204t85-203l147-146q83-83 203-83 121 0 204 85l206 207q83 83 83 203 0 123-88 209l88 88q86-88 208-88 120 0 204 84l208 208q84 84 84 204z"/></svg>
-                </span>  
-                <span class="visuallyhidden">Copy the link to this folder</span>
-            </a>
-          </span>
-          <span class="type">folder</span>
-        </h3>
+          <h3>
+              <div class="title-wrapper">
+                  <span class="name">Plants</span>
+                  <a class="title-link" title="Copy the link to this password">
+                        <span class="svg-icon light">
+                            <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1520 1216q0-40-28-68l-208-208q-28-28-68-28-42 0-72 32 3 3 19 18.5t21.5 21.5 15 19 13 25.5 3.5 27.5q0 40-28 68t-68 28q-15 0-27.5-3.5t-25.5-13-19-15-21.5-21.5-18.5-19q-33 31-33 73 0 40 28 68l206 207q27 27 68 27 40 0 68-26l147-146q28-28 28-67zm-703-705q0-40-28-68l-206-207q-28-28-68-28-39 0-68 27l-147 146q-28 28-28 67 0 40 28 68l208 208q27 27 68 27 42 0 72-31-3-3-19-18.5t-21.5-21.5-15-19-13-25.5-3.5-27.5q0-40 28-68t68-28q15 0 27.5 3.5t25.5 13 19 15 21.5 21.5 18.5 19q33-31 33-73zm895 705q0 120-85 203l-147 146q-83 83-203 83-121 0-204-85l-206-207q-83-83-83-203 0-123 88-209l-88-88q-86 88-208 88-120 0-204-84l-208-208q-84-84-84-204t85-203l147-146q83-83 203-83 121 0 204 85l206 207q83 83 83 203 0 123-88 209l88 88q86-88 208-88 120 0 204 84l208 208q84 84 84 204z"/></svg>
+                        </span>
+                      <span class="visuallyhidden">Copy the link to this password</span>
+                  </a>
+              </div>
+              <span class="type">folder</span>
+          </h3>
         <a id="js_wsp_pwd_sidebar_close" class="js_sidebar_close dialog-close" href="#">
           <?php include('includes/svg-icons/close.php'); ?>
           <span class="visuallyhidden">Close</span>
