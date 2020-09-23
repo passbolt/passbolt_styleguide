3b468233295ac9ccf721ccb16a8053f84f917707
kawan16 <karl@passbolt.com>
2020-09-23 18:18:14 +0200

    PB-79 As LU I should see the breadcrumb

---
+++ src/react-extension/components/Password/PasswordBreadcrumb/PasswordBreadcrumb.js
@@ -0,0 +1,126 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) 2020 Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2020 Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         2.13.0
+ */
+import React, {Component} from "react";
+import {withRouter} from "react-router-dom";
+import PropTypes from "prop-types";
+import {ResourceWorkspaceFilterTypes, withResourceWorkspace} from "../../../contexts/ResourceWorkspaceContext";
+import Breadcrumbs from "../../Common/Navigation/Breadcrumbs/Breadcrumbs";
+
+
+/**
+ * The component displays a navigation breadcrumb given the applied resources filter
+ */
+class PasswordBreadcrumbs extends Component {
+
+  /**
+   * Default constructor
+   * @param props The component props
+   */
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * Returns the all items breadcrumb items
+   */
+  get allItems() {
+    return  [
+      {
+        name: 'All Items',
+        link: {
+          pathname: '/app/passwords',
+          state: {
+            filter: {
+              type: ResourceWorkspaceFilterTypes.ALL
+            }
+          }
+        }
+      }
+    ]
+  }
+
+  /**
+   * Returns the breadcrumb items for the given filter
+   */
+  getBreadcrumb() {
+    return [
+      ...this.allItems,
+      {
+        name: this.getBreadcrumbItemName(),
+        link: this.props.location
+      }
+    ]
+  }
+
+  /**
+   * Returns the main breadcrumb item name given the current filter
+   * @returns {string}
+   */
+  getBreadcrumbItemName() {
+    switch(this.props.resourceWorkspaceContext.filter.type) {
+      case ResourceWorkspaceFilterTypes.ALL: return "All items";
+      case ResourceWorkspaceFilterTypes.FAVORITE: return "Favorite";
+      case ResourceWorkspaceFilterTypes.RECENTLY_MODIFIED: return "Recently modified";
+      case ResourceWorkspaceFilterTypes.SHARED_WITH_ME: return "Shared with me";
+      case ResourceWorkspaceFilterTypes.ITEMS_I_OWN: return "Items I own";
+      case ResourceWorkspaceFilterTypes.TAG: {
+        const currentTagName = this.props.resourceWorkspaceContext.filter.payload.tag.slug;
+        return currentTagName + " (tag)";
+      }
+      case ResourceWorkspaceFilterTypes.FOLDER: {
+        const currentFolderName = this.props.resourceWorkspaceContext.filter.payload.folder.name;
+        return currentFolderName + " (folder)";
+      }
+      case ResourceWorkspaceFilterTypes.TEXT: {
+        const currentSearchText = this.props.resourceWorkspaceContext.filter.payload;
+        return "Search : " + currentSearchText;
+      }
+      default: return "";
+    }
+  }
+
+  /**
+   * Returns the current list of breadcrumb items
+   */
+  get items() {
+    switch(this.props.resourceWorkspaceContext.filter.type) {
+      case ResourceWorkspaceFilterTypes.ALL:  return this.allItems;
+      case ResourceWorkspaceFilterTypes.NONE: return [];
+      case ResourceWorkspaceFilterTypes.TEXT: {
+        const isEmptySearchText = !this.props.resourceWorkspaceContext.filter.payload;
+        return isEmptySearchText ? this.allItems : this.getBreadcrumb();
+      }
+      default: return this.getBreadcrumb();
+    }
+  }
+
+
+  /**
+   * Render the component
+   * @returns {JSX}
+   */
+  render() {
+    return (
+      <Breadcrumbs items={this.items} />
+    );
+  }
+}
+
+PasswordBreadcrumbs.propTypes = {
+  resourceWorkspaceContext: PropTypes.object,
+  location: PropTypes.object
+};
+
+export default withRouter(withResourceWorkspace(PasswordBreadcrumbs));
+
