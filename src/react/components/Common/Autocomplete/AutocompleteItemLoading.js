e4a6f80d9ef72248fcc10322a8f4880e0d90221b
Cedric Alfonsi <cedric@passbolt.com>
2020-10-13 01:47:44 +0200

    Fix share dialog autocomplete search loading state
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- src/react/components/Common/Autocomplete/AutocompleteItemLoading.js
+++ src/react/components/Common/Autocomplete/AutocompleteItemLoading.js
@@ -24,9 +24,6 @@ class AutocompleteItemLoading extends Component {
         <div className="row loading">
           <div className="main-cell-wrapper">
             <div className="main-cell">
-              <div className="avatar">
-                <img src='img/controls/loading_light.svg' alt="Loading, please wait" />
-              </div>
               <div className="info">
                 <span className="name">Loading...</span>
                 <span className="details">Please wait.</span>
