8e1b2330237651662c60de7e9fb8546a380ba484
Cedric Alfonsi <cedric@passbolt.com>
2020-10-13 17:43:35 +0200

    eslint
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- src/react-extension/components/Password/PasswordSidebar/DeleteComment.test.js
+++ src/react-extension/components/Password/PasswordSidebar/DeleteComment.test.js
@@ -49,9 +49,11 @@ describe("Delete comments", () => {
       });
 
       it('I should not be able to delete the comment', async() => {
-        // @todo the API doesn't yet support this scenario.
-        // await page.title.click();
-        // expect(page.displayCommentList.canDelete(2)).toBeTruthy();
+        /*
+         * @todo the API doesn't yet support this scenario.
+         * await page.title.click();
+         * expect(page.displayCommentList.canDelete(2)).toBeTruthy();
+         */
       });
     });
   });
