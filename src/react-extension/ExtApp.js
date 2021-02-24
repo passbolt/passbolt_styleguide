45ff9cce1d674a142c055f5110d0830a76fbefe8
kevinmuller <kevin@passbolt.com>
2021-02-24 14:45:27 +0100

    PB-5038 remove notification bar by default

--- src/react-extension/ExtApp.js
+++ src/react-extension/ExtApp.js
@@ -419,19 +419,6 @@ class ExtApp extends Component {
                         <ResourceWorkspaceContextProvider>
                           <ManageDialogs/>
                           <ManageContextualMenu/>
-                          <div className="announcement">
-                            Warning: your subscription will expire in 30 days.
-                            <a href="#">Manage subscription</a>
-                            <a className="announcement-close" role="button" href="#">
-                <span className="svg-icon close">
-                  <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"></path>
-                  </svg>
-                </span>
-                              <span className="visuallyhidden">Close</span>
-                            </a>
-                          </div>
                           <div id="container" className="page password">
                             <div id="app" className={`app ${isReady ? "ready" : ""}`} tabIndex="1000">
                               <div className="header first">
