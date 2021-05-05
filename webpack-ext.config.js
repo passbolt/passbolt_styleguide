26bb4d3c05a9fcd8ecd72a2e28630e229ef0a422
Cedric Alfonsi <cedric@passbolt.com>
2021-05-05 16:07:01 +0200

    PB-6041 Fix locales issue with the demo
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- webpack-ext.config.js
+++ webpack-ext.config.js
@@ -36,12 +36,13 @@ const config = {
     ]
   },
   plugins: [
-    new CopyPlugin([
-      {
-        from: "src/react-extension/assets/i18n/**/*.json",
+    new CopyPlugin({
+      patterns: [{
+        from: "src/locales/**/*.json",
         to: path.resolve(__dirname, "build/js/dist/"),
-      },
-    ]),
+      }],
+    }),
+
   ],
   optimization: {
     splitChunks: {
