52fc4940a7e58640db42405b8ec651f26f018354
Benjamin Monnot <benjamin@passbolt.com>
2021-06-24 14:35:02 +0200

    PB-7649 - Auto-save form (quickaccess)

---
+++ src/react-quickaccess/components/ResourceAutoSave/SaveResource.test.data.js
@@ -0,0 +1,28 @@
+/**
+ * Returns the default app context for the unit test
+ * @param appContext An existing app context
+ * @returns {any}
+ */
+import MockPort from "../../../react-extension/test/mock/MockPort";
+import UserSettings from "../../../shared/lib/Settings/UserSettings";
+import userSettingsFixture from "../../../react-extension/test/fixture/Settings/userSettings";
+
+export function defaultAppContext(appContext) {
+  const defaultAppContext = {
+    port: new MockPort(),
+    userSettings: new UserSettings(userSettingsFixture)
+  };
+  return Object.assign(defaultAppContext, appContext || {});
+}
+
+const mockTabInfo = {
+  name: "test",
+  url: "www.test.com",
+  username: "test@passbolt.com",
+  password: "test@passbolt.com"
+};
+
+export const mockResults = {
+  "passbolt.auto-save.get-info": mockTabInfo,
+  "passbolt.resources.create": {}
+};
