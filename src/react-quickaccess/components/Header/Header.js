e2fc4c6de952f3c486885cb78c80f80d9666df2b
Benjamin Monnot <benjamin@passbolt.com>
2021-08-12 10:49:50 +0200

    PB-8008 - Remove the detach button inside the quickaccess

--- src/react-quickaccess/components/Header/Header.js
+++ src/react-quickaccess/components/Header/Header.js
@@ -45,16 +45,7 @@ class Header extends React.Component {
                 </svg>
               </span>
             </a>
-             <a role="button" className={`option-link button button-icon`} onClick={this.handleLogoutClick} title={this.translate("detach")}>
-              <span className="visually-hidden"><Trans>detach</Trans></span>
-              <span className="fa icon">
-                  <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="external-link-alt" className="svg-inline--fa fa-external-link-alt fa-w-16" role="img" viewBox="0 0 512 512" style={{transform: "rotate(270deg)"}}>
-                    <path d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z"/>
-                  </svg>
-              </span>
-            </a>
           </span>
-
         }
       </div>
     );
