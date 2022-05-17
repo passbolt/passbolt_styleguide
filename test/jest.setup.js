a95aace4db83fabcb8855a2a54ca8f776089a1bd
Cedric Alfonsi <cedric@passbolt.com>
2022-05-17 14:13:02 +0200

    PB-15676 Update depdencies and rebuild
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -6,3 +6,9 @@ global.console = {
   error: jest.fn(),
   warning: jest.fn()
 };
+
+/*
+ * Fix jest-webextension-mock after upgrading webextension-polyfill to 0.9.0
+ * @see https://github.com/clarkbw/jest-webextension-mock/issues/149#issuecomment-1116558554
+ */
+chrome.runtime.id = "test id";
