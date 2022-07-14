1b755ce0edb944d689ebbc25b02b5d01a89d19be
Cedric Alfonsi <cedric@passbolt.com>
2022-07-14 15:43:47 +0200

    PB-17029 Add og description
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -84,6 +84,8 @@ export const parameters = {
         'Foundations',
         'Passbolt Components',
       ],
+      method: (a, b) =>
+        a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
     },
   },
   controls: {
