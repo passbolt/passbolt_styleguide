2322d9bbc7ef7a8d426922184a58bfaa828532df
Stéphane Loegel <stephane@passbolt.com>
2022-11-10 14:41:26 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/shared/models/subscription/SubscriptionModel.js
+++ src/shared/models/subscription/SubscriptionModel.js
@@ -26,7 +26,14 @@ class SubscriptionModel {
     this.email = subscriptionDto ? "email" in subscriptionDto ? subscriptionDto.email : "N/A" : "";
     this.expiry  = subscriptionDto ? subscriptionDto.expiry : null;
     this.created  = subscriptionDto ? subscriptionDto.created : null;
-    this.data  = subscriptionDto && "data" in subscriptionDto ? subscriptionDto.data : null;
+  constructor(subscriptionDto) {
+    this.customerId = subscriptionDto?.customer_id || "";
+    this.subscriptionId = subscriptionDto?.subscription_id || "N/A";
+    this.users = subscriptionDto?.users || null;
+    this.email = subscriptionDto?.email || "N/A";
+    this.expiry  = subscriptionDto?.expiry || null;
+    this.created  = subscriptionDto?.created || null;
+    this.data  = subscriptionDto?.data || null;
   }
 }
 
