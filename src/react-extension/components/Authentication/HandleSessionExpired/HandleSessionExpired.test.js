adecefec698025bb3cdd95774f921d8a01f145f7
Benjamin Monnot <benjamin@passbolt.com>
2023-04-24 11:29:11 +0000

    PB-21564 - Application should be aware of authentication status as soon as the...

--- src/react-extension/components/Authentication/HandleSessionExpired/HandleSessionExpired.test.js
+++ src/react-extension/components/Authentication/HandleSessionExpired/HandleSessionExpired.test.js
@@ -21,45 +21,23 @@ import NotifyExpiredSession from "../NotifyExpiredSession/NotifyExpiredSession";
 
 beforeEach(() => {
   jest.resetModules();
+  jest.clearAllMocks();
 });
 
-describe("As LU I should handle session expired", () => {
-  const props = defaultProps(); // The props to pass
-  const mockContextRequest = implementation => jest.spyOn(props.context, 'onCheckIsAuthenticatedRequested').mockImplementation(implementation);
+describe("HandleSessionExpired", () => {
+  describe("As LU I should handle session expired for ApiApp", () => {
+    const props = defaultProps(); // The props to pass
+    /**
+     * I should see the session expired dialog
+     */
+    beforeEach(() => {
+      new HandleSessionExpiredPage(props);
+    });
 
-  /**
-   * I should see the session expired dialog
-   */
-  beforeEach(() => {
-    jest.useFakeTimers();
-    new HandleSessionExpiredPage(props);
-  });
-
-  afterEach(() => {
-    jest.clearAllTimers();
-  });
-
-  it('As LU I should have no change if the session is not expired', () => {
-    // Mock the request function to make it the expected result
-    const requestMockImpl = () => true;
-    mockContextRequest(requestMockImpl);
-    expect(props.context.onCheckIsAuthenticatedRequested).not.toBeCalled();
-    // Fast-forward until all timers have been executed
-    jest.runOnlyPendingTimers();
-    expect(props.context.onCheckIsAuthenticatedRequested).toHaveBeenCalled();
-    expect(props.dialogContext.open).not.toBeCalled();
-  });
-
-
-  it('As LU I should open a dialog if the session is expired',  async() => {
-    const requestMockImpl = () => false;
-    mockContextRequest(requestMockImpl);
-    // Fast-forward until all timers have been executed
-    jest.runOnlyPendingTimers();
-    // Need 2 promises resolved to open the dialog
-    await Promise.resolve();
-    await Promise.resolve();
-    expect(props.context.onCheckIsAuthenticatedRequested).toHaveBeenCalled();
-    expect(props.dialogContext.open).toHaveBeenCalledWith(NotifyExpiredSession);
+    it('As LU I should add a callback for session expired in the app context', () => {
+      expect.assertions(2);
+      expect(props.context.onExpiredSession).toHaveBeenCalledWith(expect.any(Function));
+      expect(props.dialogContext.open).toHaveBeenCalledWith(NotifyExpiredSession);
+    });
   });
 });
