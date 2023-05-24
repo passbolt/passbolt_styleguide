e30311471ff7402ef1aff87aca5c6fb8f6db169c
Diego Lendoiro <diego@passbolt.com>
2023-05-24 18:28:59 +0200

    Fix npmrc generation

--- .gitlab-ci/scripts/bin/publish.sh
+++ .gitlab-ci/scripts/bin/publish.sh
@@ -9,11 +9,9 @@ CI_SCRIPTS_DIR=$(dirname "$0")/..
 # shellcheck source=.gitlab-ci/scripts/lib/version-check.sh
 source "$CI_SCRIPTS_DIR"/lib/version-check.sh
 
-{
-  echo //registry.npmjs.com/:_authToken="$NPM_PUBLISH_TOKEN";
-  echo email=diego@passbolt.com;
-  echo always-auth=true
-} >> .npmrc
+echo //registry.npmjs.com/:_authToken="$NPM_PUBLISH_TOKEN" > .npmrc
+echo email=diego@passbolt.com >> .npmrc
+echo always-auth=true >> .npmrc
 
 if is_release_candidate "$CI_COMMIT_TAG"; then
   npm publish --tag next
