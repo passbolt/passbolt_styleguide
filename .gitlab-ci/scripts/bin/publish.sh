1dc687dbfe524ada84b55a6c38c5936c50a56c92
Diego Lendoiro <diego@passbolt.com>
2023-05-24 19:08:36 +0200

    Correct npmjs host

--- .gitlab-ci/scripts/bin/publish.sh
+++ .gitlab-ci/scripts/bin/publish.sh
@@ -9,8 +9,8 @@ CI_SCRIPTS_DIR=$(dirname "$0")/..
 # shellcheck source=.gitlab-ci/scripts/lib/version-check.sh
 source "$CI_SCRIPTS_DIR"/lib/version-check.sh
 
-echo //registry.npmjs.com/:_authToken="$NPM_PUBLISH_TOKEN" > .npmrc
-echo email=diego@passbolt.com >> .npmrc
+echo //registry.npmjs.org/:_authToken="$NPM_PUBLISH_TOKEN" > .npmrc
+echo email="$NPM_PUBLISH_EMAIL" >> .npmrc
 echo always-auth=true >> .npmrc
 
 if is_release_candidate "$CI_COMMIT_TAG"; then
