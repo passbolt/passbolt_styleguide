66a2dd9623275156014d4e476b5deb06a224e57b
Antony Bartolomucci <antony@passbolt.com>
2023-06-13 09:11:03 +0000

    PB-24646 - As LU, I should see colored passwords for a better visibility

--- src/react-quickaccess/components/ResourceViewPage/ResourceViewPage.js
+++ src/react-quickaccess/components/ResourceViewPage/ResourceViewPage.js
@@ -181,6 +181,33 @@ class ResourceViewPage extends React.Component {
   }
 
   /**
+   * Format the given password into separate character elements.
+   * Each character is wrapped in a <span> element.
+   * Alphabetic characters are left as they are, numerical characters get the "digit" CSS class, and all others get the "special-char" CSS class.
+   * @param {string} password The password to be formatted
+   * @returns {JSX.Element} A JSX element containing the formatted password
+   */
+  formatPassword(password) {
+    const characters = Array.from(password);
+
+    return (
+      <>
+      {characters.map((char, index) => {
+        if (/[\p{L}\p{N}]/u.test(char)) {
+          if (/\p{N}/u.test(char)) {
+            return <span key={index} className="digit">{char}</span>;
+          } else {
+            return <span key={index}>{char}</span>;
+          }
+        } else {
+          return <span key={index} className="special-char">{char}</span>;
+        }
+      })}
+    </>
+    );
+  }
+
+  /**
    * Get the password property from a secret plaintext object.
    * @param {string|object} plaintextDto The secret plaintext
    * @returns {string}
@@ -349,7 +376,7 @@ class ResourceViewPage extends React.Component {
                   title={isPasswordPreviewed ? this.state.previewedPassword : "secret"}
                   onClick={this.handleCopyPasswordClick}>
                   {isPasswordPreviewed &&
-                    <span>{this.state.previewedPassword}</span>
+                    <span>{this.formatPassword(this.state.previewedPassword)}</span>
                   }
                   {!isPasswordPreviewed &&
                     <span className="visually-hidden"><Trans>Copy to clipboard</Trans></span>
