fdd68d45e053342830e8bd5520d8dd91d20a645c
Benjamin Monnot <benjamin@passbolt.com>
2023-10-26 13:18:47 +0200

    PB-26002 As a user downloading my recovery kit I want to be warned about the critical character of this asset

--- src/react-extension/components/Authentication/DownloadRecoveryKit/DownloadRecoveryKit.js
+++ src/react-extension/components/Authentication/DownloadRecoveryKit/DownloadRecoveryKit.js
@@ -17,11 +17,59 @@ import PropTypes from "prop-types";
 
 class DownloadRecoveryKit extends Component {
   /**
+   * Constructor
+   * @param {Object} props
+   */
+  constructor(props) {
+    super(props);
+    this.state = this.defaultState;
+    this.bindEventHandlers();
+  }
+
+  /**
+   * Bind event handlers
+   * @returns {void}
+   */
+  bindEventHandlers() {
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleNext = this.handleNext.bind(this);
+    this.handleDownload = this.handleDownload.bind(this);
+  }
+
+  /**
+   * Get default state
+   * @returns {*}
+   */
+  get defaultState() {
+    return {
+      isStoredRecoveryKitSafe: false
+    };
+  }
+  /**
    * ComponentDidMount
    * @return {void}
    */
   componentDidMount() {
     this.props.onDownload();
+  }
+
+  /**
+   * Handle form input changes.
+   * @params {ReactEvent} The react event
+   */
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.checked;
+    const name = target.name;
+    this.setState({[name]: value});
+  }
+
+  /**
+   * Return true if the form has some validation error
+   * @returns {boolean}
+   */
+  hasValidationError() {
+    return this.state.usernameError !== null || this.state.agreedTermsError;
   }
 
   /**
@@ -39,30 +87,49 @@ class DownloadRecoveryKit extends Component {
   }
 
   /**
+   * Should input be disabled? True if state is loading or processing
+   * @returns {boolean}
+   */
+  hasAllInputDisabled() {
+    return !this.state.isStoredRecoveryKitSafe;
+  }
+
+  /**
    * Render the component
    * @returns {JSX.Element}
    */
   render() {
     return (
       <div className="generate-key-feedback">
-        <h1><Trans>Keep your recovery kit in a safe place.</Trans></h1>
+        <h1><Trans>Store your recovery kit in a safe place.</Trans></h1>
+        <p>
+          <strong>
+            <Trans>You will need this recovery kit later to access your account (for example on a new device).</Trans>
+          </strong>
+        </p>
         <p>
           <Trans>A download of your recovery kit, containing your secret key, has automatically started.</Trans>
-          &nbsp;
-          <Trans>Make sure you store it in a safe place. You may need it later.</Trans>
         </p>
+        <div className="input checkbox">
+          <input type="checkbox" name="isStoredRecoveryKitSafe" checked={this.state.isStoredRecoveryKitSafe} onChange={this.handleInputChange}
+            id="checkbox-recovery-kit"/>
+          <label htmlFor="checkbox-recovery-kit">
+            <Trans>I safely stored my recovery kit.</Trans>
+          </label>
+        </div>
         <div className="form-actions">
           <button
             type="submit"
             className={`button primary big full-width`}
-            onClick={this.handleNext.bind(this)}>
+            disabled={this.hasAllInputDisabled()}
+            onClick={this.handleNext}>
             <Trans>Next</Trans>
           </button>
           <button
             className="link"
             type="button"
             id="download-kit"
-            onClick={this.handleDownload.bind(this)}>
+            onClick={this.handleDownload}>
             <Trans>Download the kit again!</Trans>
           </button>
         </div>
