f738e6e259bdefaa6a4cf51e194bbdf8ca374b05
Pierre Colart <pierre@passbolt.com>
2023-11-15 06:55:27 +0000

    PB-27835 - As signed-in user configuring MFA TOTP I can see the TOTP secret so...

--- src/react-extension/components/MFA/DisplayProviderList/DisplayProviderList.js
+++ src/react-extension/components/MFA/DisplayProviderList/DisplayProviderList.js
@@ -21,6 +21,7 @@ import {MfaSettingsWorkflowStates, Providers, withMfa} from "../../../contexts/M
 import MfaProviders from "./MfaProviders.data";
 import {withRbac} from "../../../../shared/context/Rbac/RbacContext";
 import {uiActions} from "../../../../shared/services/rbacs/uiActionEnumeration";
+import {withNavigationContext} from "../../../contexts/NavigationContext";
 
 /**
  * This component will display the Mfa provider enabled/disabled and allowed
@@ -118,7 +119,7 @@ class DisplayProviderList extends Component {
   handleProviderClick(provider) {
     const mfaUserSettings = this.props.mfaContext.getMfaUserSettings();
     this.props.mfaContext.setProvider(provider);
-    if (mfaUserSettings[provider]) {
+    if (mfaUserSettings[provider] && provider !== "duo") {
       this.props.mfaContext.navigate(MfaSettingsWorkflowStates.VIEWCONFIGURATION);
     } else {
       switch (provider) {
@@ -126,7 +127,7 @@ class DisplayProviderList extends Component {
           this.props.mfaContext.navigate(MfaSettingsWorkflowStates.TOTPOVERVIEW);
           break;
         case Providers.DUO:
-          //Todo
+          this.props.navigationContext.onGoToUserSettingsDuoSetupRequested();
           break;
         case Providers.YUBIKEY:
           this.props.mfaContext.navigate(MfaSettingsWorkflowStates.SETUPYUBIKEY);
@@ -202,6 +203,7 @@ DisplayProviderList.propTypes = {
   t: PropTypes.func, // The translation function
   mfaContext: PropTypes.object, // The mfa context
   rbacContext: PropTypes.any, // The role based access control context
+  navigationContext: PropTypes.any, // The application navigation context
 };
 
-export default withAppContext(withMfa(withRbac(withTranslation("common")(DisplayProviderList))));
+export default withAppContext(withMfa(withRbac(withNavigationContext(withTranslation("common")(DisplayProviderList)))));
