f8ff9efbc393325e21ccf6aa7958dfd6e7b11172
Cedric Alfonsi <cedric@passbolt.com>
2024-02-27 07:47:36 +0000

    PB-30015 - Remove unused collection constructor second optional parameter

--- src/shared/models/entity/abstract/entityCollection.test.js
+++ src/shared/models/entity/abstract/entityCollection.test.js
@@ -62,44 +62,107 @@ class TestEntityCollection extends Entity {
  */
 
 describe("EntityCollection", () => {
-  it("constructor and getters works", () => {
-    const dto = [{name: 'first'}, {name: 'second'}, {name: 'first'}];
-    const entities = [];
-    entities.push(new TestEntity({name: 'first'}));
-    entities.push(new TestEntity({name: 'second'}));
-    entities.push(new TestEntity({name: 'first'}));
-
-    const collection = new EntityCollection(dto, entities);
-    expect(collection.items.length).toBe(3);
-    expect(collection.toDto()).toEqual(dto);
-
-    // Push
-    collection.push(new TestEntity({name: 'fourth'}));
-    expect(collection.items.length).toBe(4);
-    expect(collection.items[0].name).toEqual('first');
-    expect(collection.items[1].name).toEqual('second');
-    expect(collection.items[2].name).toEqual('first');
-    expect(collection.items[4]).toEqual(undefined);
-
-    // Iterator tests
-    for (const item of collection) {
-      expect(item.name).toEqual('first');
-      break;
-    }
-
-    // find all
-    expect(collection.getAll('name', 'first').length).toBe(2);
-    expect(collection.getAll('name', 'second').length).toBe(1);
-    expect(collection.getAll('name', 'third').length).toBe(0);
-
-    // find first
-    expect(collection.getFirst('name', 'first').toDto()).toEqual({name: 'first'});
-    expect(collection.getFirst('name', 'third')).toBe(undefined);
+  describe("EntityCollection::constructor", () => {
+    it("constructor and getters works with empty collection", () => {
+      const collection = new EntityCollection([]);
+      expect.assertions(3);
+      expect(collection.length).toBe(0);
+      expect(collection.items).toEqual([]);
+      expect(collection.items[0]).toEqual(undefined);
+    });
   });
 
-  it("constructor and getters works with empty collection", () => {
-    const collection = new EntityCollection([]);
-    expect(collection.length).toBe(0);
-    expect(collection.items).toEqual([]);
+  describe("EntityCollection::push", () => {
+    it("should push items to the collection", () => {
+      const collection = new EntityCollection();
+      collection.push(new TestEntity({name: 'first'}));
+      collection.push(new TestEntity({name: 'second'}));
+      collection.push(new TestEntity({name: 'first'}));
+
+      expect.assertions(4);
+      expect(collection.items.length).toBe(3);
+      expect(collection.items[0].name).toEqual('first');
+      expect(collection.items[1].name).toEqual('second');
+      expect(collection.items[2].name).toEqual('first');
+    });
+
+    it("should allow to push undefined item to the collection", () => {
+      const collection = new EntityCollection();
+      collection.push();
+      expect.assertions(2);
+      expect(collection.items.length).toBe(1);
+      expect(collection.items[0]).toBeUndefined();
+    });
+  });
+
+  describe("EntityCollection::toDto", () => {
+    it("should return collection dto", () => {
+      const collection = new EntityCollection();
+      collection.push(new TestEntity({name: 'first'}));
+      collection.push(new TestEntity({name: 'second'}));
+      collection.push(new TestEntity({name: 'first'}));
+
+      expect(collection.items.length).toBe(3);
+      const expectedDtos = [{name: 'first'}, {name: 'second'}, {name: 'first'}];
+
+      expect.assertions(2);
+      expect(collection.toDto()).toEqual(expectedDtos);
+    });
+  });
+
+  describe("EntityCollection::getFirst", () => {
+    it("should get the first item which the given property is matching the given value", () => {
+      const collection = new EntityCollection();
+      collection.push(new TestEntity({name: 'first'}));
+      collection.push(new TestEntity({name: 'second'}));
+      collection.push(new TestEntity({name: 'first'}));
+
+      expect.assertions(3);
+      expect(collection.getFirst('name', 'first').toDto()).toEqual({name: 'first'});
+      expect(collection.getFirst('name', 'second').toDto()).toEqual({name: 'second'});
+      expect(collection.getFirst('name', 'third')).toBe(undefined);
+    });
+  });
+
+  describe("EntityCollection::getAll", () => {
+    it("should get all the items which the given property is matching the given value", () => {
+      const collection = new EntityCollection();
+      collection.push(new TestEntity({name: 'first'}));
+      collection.push(new TestEntity({name: 'second'}));
+      collection.push(new TestEntity({name: 'first'}));
+
+      expect.assertions(7);
+      const firstCollection = collection.getAll('name', 'first');
+      expect(firstCollection.length).toBe(2);
+      expect(firstCollection[0].toDto()).toEqual({name: 'first'});
+      expect(firstCollection[1].toDto()).toEqual({name: 'first'});
+      expect(collection.getFirst('name', 'second').toDto()).toEqual({name: 'second'});
+      const secondCollection = collection.getAll('name', 'second');
+      expect(secondCollection.length).toBe(1);
+      expect(secondCollection[0].toDto()).toEqual({name: 'second'});
+      const thirdCollection = collection.getAll('name', 'third');
+      expect(thirdCollection.length).toBe(0);
+    });
+  });
+
+  describe("EntityCollection::items::iterator", () => {
+    it("should get all the items which the given property is matching the given value", () => {
+      const collection = new EntityCollection();
+      collection.push(new TestEntity({name: 'first'}));
+      collection.push(new TestEntity({name: 'second'}));
+
+      expect.assertions(2);
+      let i = 0;
+      for (const item of collection) {
+        if (i === 0) {
+          expect(item.name).toEqual('first');
+        } else if (i === 1) {
+          expect(item.name).toEqual('second');
+        } else {
+          expect(true).toBeFalsy();
+        }
+        i++;
+      }
+    });
   });
 });
