04d4ca5b5f9a7a809e5a6de915770503aab1c6a5
Benjamin Monnot <benjamin@passbolt.com>
2024-04-30 10:24:13 +0200

    PB-33171 Simulate user event on autofill

--- src/react-web-integration/lib/InForm/InFormManager.js
+++ src/react-web-integration/lib/InForm/InFormManager.js
@@ -18,6 +18,7 @@ import InFormMenuField from "./InformMenuField";
 import InFormCredentialsFormField from "./InFormCredentialsFormField";
 import DomUtils from "../Dom/DomUtils";
 import debounce from "debounce-promise";
+import UserEventsService from "../User/UserEventsService";
 
 /**
  * Manages the in-form web integration including call-to-action and menu
@@ -259,22 +260,26 @@ class InFormManager {
       const isUsernameType = currentFieldType === 'username';
       const isPasswordType = currentFieldType === 'password';
       if (!isUsernameType) {
-        this.lastCallToActionFieldClicked.autofill(password);
+        // Simulate a user to autofill the password field
+        UserEventsService.autofill(this.lastCallToActionFieldClicked.field, password);
         // Get username fields and find the one with the lowest common ancestor
         const usernameFields = this.callToActionFields
           .filter(callToActionField => callToActionField.fieldType === 'username');
         const usernameField = DomUtils.getFieldWithLowestCommonAncestor(this.lastCallToActionFieldClicked.field, usernameFields);
         if (usernameField) {
-          usernameField.autofill(username);
+          // Simulate a user to autofill the username field
+          UserEventsService.autofill(usernameField.field, username);
         }
       } else if (!isPasswordType) {
-        this.lastCallToActionFieldClicked.autofill(username);
+        // Simulate a user to autofill the username field
+        UserEventsService.autofill(this.lastCallToActionFieldClicked.field, username);
         // Get password fields and find the one with the lowest common ancestor
         const passwordFields = this.callToActionFields
           .filter(callToActionField => callToActionField.fieldType === 'password');
         const passwordField = DomUtils.getFieldWithLowestCommonAncestor(this.lastCallToActionFieldClicked.field, passwordFields);
         if (passwordField) {
-          passwordField.autofill(password);
+          // Simulate a user to autofill the password field
+          UserEventsService.autofill(passwordField.field, password);
         }
       }
     });
@@ -288,7 +293,7 @@ class InFormManager {
       const passwordFields = this.callToActionFields
         .filter(callToActionField => callToActionField.fieldType === 'password');
       // Autofill only empty passwords field
-      passwordFields.forEach(callToActionField => !callToActionField.field.value && callToActionField.autofill(password));
+      passwordFields.forEach(callToActionField => !callToActionField.field.value && UserEventsService.autofill(callToActionField.field, password));
       this.menuField.removeMenuIframe();
       // Listen the auto-save on the appropriate form field
       const formField = this.credentialsFormFields.find(formField => formField.field.contains(this.lastCallToActionFieldClicked.field));
