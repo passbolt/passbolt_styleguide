fbfb4d2bfcbdf4d6ce7674ee5fc95da7e594c3d5
Stéphane Loegel <stephane@passbolt.com>
2024-05-10 09:56:18 +0200

    PB-33264: add coverage on ApplicationEntity for healthcheck

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,5 +1,8 @@
+import "./matchers/extendExpect";
+
 // Disable console debug, warning and error while executing the tests.
 // Keep console log as it can be useful for testing.
+
 global.console = {
   ...console,
   debug: jest.fn(),
