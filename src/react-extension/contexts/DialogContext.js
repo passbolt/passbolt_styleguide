c7a248fdb0b1b8cd20160d9d9b3ae4dffad17651
Benjamin Monnot <benjamin@passbolt.com>
2024-06-20 09:27:47 +0200

    PB-33824 As a user I should not see other dialog open except the session expired

--- src/react-extension/contexts/DialogContext.js
+++ src/react-extension/contexts/DialogContext.js
@@ -50,7 +50,8 @@ export default class DialogContextProvider extends React.Component {
         this.setState({dialogs: [...this.state.dialogs, {key: dialogKey, Dialog, DialogProps}]});
         return dialogKey;
       },
-      close: dialogKey => this.setState({dialogs: this.state.dialogs.filter(dialog => dialogKey !== dialog.key)})
+      close: dialogKey => this.setState({dialogs: this.state.dialogs.filter(dialog => dialogKey !== dialog.key)}),
+      closeAll: () => this.setState({dialogs: []})
     };
   }
 
