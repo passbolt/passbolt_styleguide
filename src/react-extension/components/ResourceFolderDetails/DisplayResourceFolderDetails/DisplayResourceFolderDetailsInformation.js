1b3dd84a8631679d84fa26a5e337081268127545
pierre <pierre@passbolt.com>
2024-06-25 13:21:09 +0200

    PB-33816 - root should not have shared icon

--- src/react-extension/components/ResourceFolderDetails/DisplayResourceFolderDetails/DisplayResourceFolderDetailsInformation.js
+++ src/react-extension/components/ResourceFolderDetails/DisplayResourceFolderDetails/DisplayResourceFolderDetailsInformation.js
@@ -162,7 +162,7 @@ class DisplayResourceFolderDetailsInformation extends React.Component {
               <span className="label"><Trans>Location</Trans></span>
               <span className="value">
                 <button type="button" onClick={this.handleFolderParentClickEvent} disabled={!this.props.context.folders} className="link no-border folder-link">
-                { this.folder.personal ? <Icon name="folder"/> : <Icon name="folder-shared"/> }
+                { this.folder.personal || this.folder.folder_parent_id == null ? <Icon name="folder"/> : <Icon name="folder-shared"/> }
                   <span>{folderParentName}</span>
                 </button>
               </span>
