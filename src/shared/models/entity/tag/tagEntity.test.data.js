bcb9c17119db9d88b66618a1142b87336a3a94e4
Stéphane Loegel <stephane@passbolt.com>
2024-09-09 08:45:52 +0000

    Feature/pb 35170 wp2 25 unnecessary quick a api call for filtering resources   filter by tags

---
+++ src/shared/models/entity/tag/tagEntity.test.data.js
@@ -0,0 +1,31 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         4.9.4
+ */
+
+export const minimalTagDto = (data = {}) => ({
+  slug: "minimal-tag",
+  ...data
+});
+
+export const defaultTagDto = (data = {}) => ({
+  id: crypto.randomUUID(),
+  slug: "personal-tag",
+  is_shared: false,
+  ...data
+});
+
+export const sharedTagDto = (data = {}) => defaultTagDto({
+  slug: "#shared-tag",
+  is_shared: true,
+  ...data
+});
