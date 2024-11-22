f7a46b51dde35918a108f02f6e9ca04abc1fcc39
Stéphane Loegel <stephane@passbolt.com>
2024-11-22 13:58:37 +0100

    PB-36950 - WP4-2.10 Migrate RadioButton to v5

--- src/react-extension/components/Common/RadioButton/RadioButton.test.stories.js
+++ src/react-extension/components/Common/RadioButton/RadioButton.test.stories.js
@@ -18,42 +18,40 @@ export default {
   component: "RadioButton"
 };
 
-
-const Template = () =>
-  <div className="radiolist" style={{display: "flex", flexWrap: "wrap", gap: "1.6rem"}}>
-    <div className="input radio" style={{width: "25%"}}>
-      <input type="radio" checked={false} disabled={false} readOnly={true}/>
-      <label/>
+export const DefaultRadioButton = {
+  render: () =>
+    <div className="radiolist" style={{display: "flex", flexWrap: "wrap", gap: "1.6rem"}}>
+      <div className="input radio" style={{width: "25%"}}>
+        <input type="radio" checked={false} disabled={false} readOnly={true}/>
+        <label/>
+      </div>
+      <div className="input radio" style={{width: "25%"}}>
+        <input type="radio" checked={false} disabled={true} readOnly={true}/>
+        <label/>
+      </div>
+      <div className="input radio" style={{width: "25%"}}>
+        <input type="radio" checked={true} disabled={false} readOnly={true}/>
+        <label/>
+      </div>
+      <div className="input radio" style={{width: "25%"}}>
+        <input type="radio" checked={true} disabled={true} readOnly={true}/>
+        <label/>
+      </div>
+      <div className="input radio" style={{width: "25%"}}>
+        <input type="radio" id="label1" checked={false} disabled={false} readOnly={true}/>
+        <label htmlFor="label1">Label</label>
+      </div>
+      <div className="input radio" style={{width: "25%"}}>
+        <input type="radio" id="label2" checked={false} disabled={true} readOnly={true}/>
+        <label htmlFor="label2">Label</label>
+      </div>
+      <div className="input radio" style={{width: "25%"}}>
+        <input type="radio" id="label3" checked={true} disabled={false} readOnly={true}/>
+        <label htmlFor="label3">Label</label>
+      </div>
+      <div className="input radio" style={{width: "25%"}}>
+        <input type="radio" id="label4" checked={true} disabled={true} readOnly={true}/>
+        <label htmlFor="label4">Label</label>
+      </div>
     </div>
-    <div className="input radio" style={{width: "25%"}}>
-      <input type="radio" checked={false} disabled={true} readOnly={true}/>
-      <label/>
-    </div>
-    <div className="input radio" style={{width: "25%"}}>
-      <input type="radio" checked={true} disabled={false} readOnly={true}/>
-      <label/>
-    </div>
-    <div className="input radio" style={{width: "25%"}}>
-      <input type="radio" checked={true} disabled={true} readOnly={true}/>
-      <label/>
-    </div>
-    <div className="input radio" style={{width: "25%"}}>
-      <input type="radio" id="label1" checked={false} disabled={false} readOnly={true}/>
-      <label htmlFor="label1">Label</label>
-    </div>
-    <div className="input radio" style={{width: "25%"}}>
-      <input type="radio" id="label2" checked={false} disabled={true} readOnly={true}/>
-      <label htmlFor="label2">Label</label>
-    </div>
-    <div className="input radio" style={{width: "25%"}}>
-      <input type="radio" id="label3" checked={true} disabled={false} readOnly={true}/>
-      <label htmlFor="label3">Label</label>
-    </div>
-    <div className="input radio" style={{width: "25%"}}>
-      <input type="radio" id="label4" checked={true} disabled={true} readOnly={true}/>
-      <label htmlFor="label4">Label</label>
-    </div>
-  </div>
-  ;
-
-export const Default = Template.bind({});
+};
