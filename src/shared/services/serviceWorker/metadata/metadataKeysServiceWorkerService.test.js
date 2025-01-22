8e779c50f0a2c95c587ad870f01a2ac9b683fdf2
Cedric Alfonsi <cedric@passbolt.com>
2025-01-22 09:48:18 +0100

    PB-38194 - WP5-2.10 Implement generate function on MetadataKeysServiceWorkerService to support new metadata key generation in content types metadata keys administration page
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- src/shared/services/serviceWorker/metadata/metadataKeysServiceWorkerService.test.js
+++ src/shared/services/serviceWorker/metadata/metadataKeysServiceWorkerService.test.js
@@ -13,9 +13,14 @@
  */
 
 import MockPort from "../../../../react-extension/test/mock/MockPort";
-import MetadataKeysServiceWorkerService, {METADATA_KEYS_FIND_ALL_EVENT} from "./metadataKeysServiceWorkerService";
+import MetadataKeysServiceWorkerService, {
+  METADATA_KEYS_FIND_ALL_EVENT,
+  METADATA_KEYS_GENERATE_EVENT
+} from "./metadataKeysServiceWorkerService";
 import {defaultMetadataKeysDtos} from "../../../models/entity/metadata/metadataKeysCollection.test.data";
 import MetadataKeysCollection from "../../../models/entity/metadata/metadataKeysCollection";
+import {ExternalGpgKeyEntityFixtures} from "../../../models/entity/gpgkey/externalGpgKeyEntity.test.fixtures";
+import ExternalGpgKeyPairEntity from "../../../models/entity/gpgkey/external/externalGpgKeyPairEntity";
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -38,6 +43,23 @@ describe("MetadataKeysServiceWorkerService", () => {
       expect(port.request).toHaveBeenCalledWith(METADATA_KEYS_FIND_ALL_EVENT);
       expect(settings).toBeInstanceOf(MetadataKeysCollection);
       expect(settings.toDto()).toEqual(dto);
+    });
+  });
+
+  describe("::generatePrivate", () => {
+    it("requests the service worker with the expected event and return the request output.", async() => {
+      expect.assertions(4);
+      const externalGpgKeyDto = ExternalGpgKeyEntityFixtures.minimal_dto;
+      const dto = {
+        public_key: externalGpgKeyDto,
+        private_key: externalGpgKeyDto
+      };
+      jest.spyOn(port, "request").mockReturnValue(dto);
+      const externalGpgKeyPair = await service.generatePrivate();
+      expect(port.request).toHaveBeenCalledWith(METADATA_KEYS_GENERATE_EVENT);
+      expect(externalGpgKeyPair).toBeInstanceOf(ExternalGpgKeyPairEntity);
+      expect(externalGpgKeyPair.publicKey.armoredKey).toEqual(dto.public_key.armored_key);
+      expect(externalGpgKeyPair.privateKey.armoredKey).toEqual(dto.private_key.armored_key);
     });
   });
 });
