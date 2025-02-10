3747748797ded4e5854a328afb5c2b63120021b3
Stéphane Loegel <stephane@passbolt.com>
2025-02-10 15:34:58 +0100

    PB-38830 - WP4-9.18 Migrate Provide Account Recovery Organization Key dialog to v5

--- src/react-extension/components/Administration/ProvideAccountRecoveryOrganizationKey/ProvideAccountRecoveryOrganizationKey.test.stories.js
+++ src/react-extension/components/Administration/ProvideAccountRecoveryOrganizationKey/ProvideAccountRecoveryOrganizationKey.test.stories.js
@@ -12,30 +12,20 @@
  * @since         3.6.0
  */
 
-import {MemoryRouter, Route} from "react-router-dom";
-import React from "react";
 import ProvideAccountRecoveryOrganizationKey from "./ProvideAccountRecoveryOrganizationKey";
 import userSettingsFixture from "../../../test/fixture/Settings/userSettings";
 import UserSettings from "../../../../shared/lib/Settings/UserSettings";
 
 export default {
   title: 'Components/Administration/ProvideAccountRecoveryOrganizationKey',
-  component: ProvideAccountRecoveryOrganizationKey
+  component: ProvideAccountRecoveryOrganizationKey,
 };
 
-
-const Template = args =>
-  <MemoryRouter initialEntries={['/']}>
-    <Route component={routerProps => <ProvideAccountRecoveryOrganizationKey {...args} {...routerProps}/>}></Route>
-  </MemoryRouter>;
-
-export const Initial = Template.bind({});
-Initial.args = {
-  context: {
-    userSettings: new UserSettings(userSettingsFixture),
+export const Initial = {
+  args: {
+    context: {
+      userSettings: new UserSettings(userSettingsFixture),
+    },
+    onClose: () => {}
   },
-  onClose: () => {}
 };
-
-
-
