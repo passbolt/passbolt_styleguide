d61fdf2fd19d9342e9500fab92e5fb4f0e784796
Benjamin Monnot <benjamin@passbolt.com>
2025-02-12 09:07:19 +0100

    PB-38861 WP4-9.47 Migrate Create User dialog to v5

--- src/react-extension/components/User/CreateUser/CreateUser.test.stories.js
+++ src/react-extension/components/User/CreateUser/CreateUser.test.stories.js
@@ -1,9 +1,5 @@
-import {MemoryRouter, Route} from "react-router-dom";
-import React from "react";
-import AppContext from "../../../../shared/context/AppContext/AppContext";
-import PropTypes from "prop-types";
 import CreateUser from "./CreateUser";
-import MockPort from "../../../test/mock/MockPort";
+import {defaultAppContext} from "../../../contexts/ExtAppContext.test.data";
 
 
 export default {
@@ -11,31 +7,11 @@ export default {
   component: CreateUser
 };
 
-const context = {
-  setContext: () => {},
-  roles: [
-    {name: "admin"},
-    {name: "user"}
-  ],
-  port: new MockPort()
-};
 
-const Template = ({context, ...args}) =>
-  <AppContext.Provider value={context}>
-    <MemoryRouter initialEntries={['/']}>
-      <Route component={routerProps => <CreateUser {...args} {...routerProps}/>}></Route>
-    </MemoryRouter>
-  </AppContext.Provider>;
 
-Template.propTypes = {
-  context: PropTypes.object,
-};
 
-export const Initial = Template.bind({});
-Initial.args = {
-  context: context,
-  onClose: () => {}
-};
-Initial.parameters = {
-  css: "api_main"
+export const Initial = {
+  args: {
+    context: defaultAppContext()
+  }
 };
