27c54c9fe9f9e17781316d2952e84cc786d7fc36
pierre <pierre@passbolt.com>
2025-02-13 15:36:30 +0100

    PB-39030 - WP6-2.8 Create secretDataV5StandaloneEntity

---
+++ src/shared/models/entity/secretData/secretDataV5StandaloneEntity.js
@@ -0,0 +1,70 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         5.0.0
+ */
+
+import TotpEntity from "../totp/totpEntity";
+import SecretDataEntity from "./secretDataEntity";
+
+class SecretDataV5StandaloneEntity extends SecretDataEntity {
+  /**
+   * @inheritDoc
+   */
+  constructor(dto, options = {}) {
+    super(dto, options);
+
+    // Associations
+    if (this._props.totp) {
+      this._totp = new TotpEntity(this._props.totp, {...options, clone: false});
+      delete this._props.totp;
+    }
+  }
+
+  /**
+   * Get the secret data default totp schema
+   * @returns {object}
+   */
+  static getSchema() {
+    return {
+      "type": "object",
+      "required": [
+        "object_type",
+        "totp"
+      ],
+      "properties": {
+        ...SecretDataEntity.getSchema().properties,
+        "totp": TotpEntity.getSchema(),
+      }
+    };
+  }
+
+  /**
+   * Get the DTO of properties managed by the form.
+   * @returns {object}
+   */
+  toDto() {
+    return {
+      ...this._props,
+      totp: this.totp.toDto(),
+    };
+  }
+
+  /**
+   * Get the totp association.
+   * @returns {TotpEntity}
+   */
+  get totp() {
+    return this._totp;
+  }
+}
+
+export default SecretDataV5StandaloneEntity;
---
+++ src/shared/models/entity/secretData/secretDataV5StandaloneEntity.test.js
@@ -0,0 +1,54 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         5.0.0
+ */
+
+import EntitySchema from "../abstract/entitySchema";
+import * as assertEntityProperty from "../../../../../test/assert/assertEntityProperty";
+import {defaultTotpDto} from "./secretDataV4DefaultTotpEntity.test.data";
+import SecretDataV5StandaloneEntity from "./secretDataV5StandaloneEntity";
+
+describe("SecretDataV5StandaloneEntity", () => {
+  describe("::getSchema", () => {
+    it("schema must validate", () => {
+      EntitySchema.validateSchema(SecretDataV5StandaloneEntity.name, SecretDataV5StandaloneEntity.getSchema());
+    });
+
+    it("validates totp property", () => {
+      expect.assertions(3);
+
+      const successScenario = [
+        {scenario: "a valid totp", value: defaultTotpDto()},
+      ];
+      const failScenario = [
+        {scenario: "The totp validation should failed.", value: defaultTotpDto({
+          algorithm: "not an algorithm"
+        })},
+      ];
+      assertEntityProperty.assertAssociation(SecretDataV5StandaloneEntity, "totp", {object_type: "PASSBOLT_SECRET_DATA"}, successScenario, failScenario);
+      assertEntityProperty.required(SecretDataV5StandaloneEntity, "totp");
+    });
+  });
+
+  describe("::constructor", () => {
+    it("constructor works if valid DTO is provided", () => {
+      expect.assertions(1);
+      const dto = {
+        object_type: "PASSBOLT_SECRET_DATA",
+        totp: defaultTotpDto()
+      };
+      const entity = new SecretDataV5StandaloneEntity(dto);
+
+      expect(entity.totp.toDto()).toStrictEqual(dto.totp);
+    });
+  });
+});
