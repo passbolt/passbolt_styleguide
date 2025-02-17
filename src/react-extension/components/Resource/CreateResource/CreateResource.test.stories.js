456e0e281e6851c8755ba73615f64ec17c2da135
Benjamin Monnot <benjamin@passbolt.com>
2025-02-17 16:35:58 +0100

    PB-39012 WP6-1.5 Implement resource header layout

--- src/react-extension/components/Resource/CreateResource/CreateResource.test.stories.js
+++ src/react-extension/components/Resource/CreateResource/CreateResource.test.stories.js
@@ -1,7 +1,5 @@
-import {MemoryRouter, Route} from "react-router-dom";
+import {MemoryRouter} from "react-router-dom";
 import React from "react";
-import AppContext from "../../../../shared/context/AppContext/AppContext";
-import PropTypes from "prop-types";
 import CreateResource from "./CreateResource";
 import MockPort from "../../../test/mock/MockPort";
 import {defaultAppContext, defaultProps} from "./CreateResource.test.data";
@@ -9,32 +7,21 @@ import {defaultAppContext, defaultProps} from "./CreateResource.test.data";
 export default {
   title: 'Components/Resource/CreateResource',
   component: CreateResource,
-  argTypes: {
-    language: {
-      control: {
-        type: 'select',
-        options: ['en-US', 'fr']
-      }
-    }
-  }
+  decorators: [(Story, {args}) =>
+    <MemoryRouter initialEntries={['/app/passwords']}>
+      <div style={{margin: "-1rem"}}>
+        <Story {...args}/>
+      </div>
+    </MemoryRouter>
+  ],
 };
 
 const mockedPort = new MockPort();
 mockedPort.addRequestListener("passbolt.resources.create", data => data);
 
-const Template = ({context, ...args})  =>
-  <AppContext.Provider value={context}>
-    <MemoryRouter initialEntries={['/']}>
-      <Route component={routerProps => <CreateResource {...args} {...routerProps}/>}></Route>
-    </MemoryRouter>
-  </AppContext.Provider>;
-
-Template.propTypes = {
-  context: PropTypes.object
-};
-
-export const PasswordInDictionary = Template.bind({});
-PasswordInDictionary.args = {
-  context: defaultAppContext({port: mockedPort}),
-  ...defaultProps()
+export const PasswordInDictionary = {
+  args: {
+    context: defaultAppContext({port: mockedPort}),
+    ...defaultProps()
+  }
 };
