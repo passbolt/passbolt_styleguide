317ec21658c0830e65ed53e0b9047cbf44602ef7
Stéphane Loegel <stephane@passbolt.com>
2025-02-20 18:00:29 +0100

    PB-39000 - WP6-4.5 Implements ResourceTypesServiceWorkerService to request the service worker for retrieving the resource types

---
+++ src/shared/services/serviceWorker/resourceTypes/resourceTypesServiceWorkerService.js
@@ -0,0 +1,37 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         4.12.0
+ */
+import ResourceTypesCollection from "../../../models/entity/resourceType/resourceTypesCollection";
+
+export const RESOURCE_TYPE_FIND_DELETED_AND_NON_DELETED_EVENT = "passbolt.metadata.find-metadata-migrate-resources-details";
+
+class ResourceTypesServiceWorkerService {
+  /**
+   * Constructor
+   * @param {port} port The browser extension background page / service worker port.
+   */
+  constructor(port) {
+    this.port = port;
+  }
+
+  /**
+   * Find all the resource types (deleted and available).
+   * @returns {Promise<ResourceTypesCollection>}
+   */
+  async findAllByDeletedAndNonDeleted() {
+    const metadataCountDetails = await this.port.request(RESOURCE_TYPE_FIND_DELETED_AND_NON_DELETED_EVENT);
+    return new ResourceTypesCollection(metadataCountDetails);
+  }
+}
+
+export default ResourceTypesServiceWorkerService;
