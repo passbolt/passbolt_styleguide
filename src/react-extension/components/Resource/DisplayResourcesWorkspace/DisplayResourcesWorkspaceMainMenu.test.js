a442df4c6d8f54b16849a2dfa2e69c5d41fb8a8c
masahiro-takusagawa-funai <masahiro-takusagawa@funaisoken.co.jp>
2025-03-17 19:05:12 +0900

    粒度変更

--- src/react-extension/components/Resource/DisplayResourcesWorkspace/DisplayResourcesWorkspaceMainMenu.test.js
+++ src/react-extension/components/Resource/DisplayResourcesWorkspace/DisplayResourcesWorkspaceMainMenu.test.js
@@ -42,152 +42,118 @@ beforeEach(() => {
 });
 
 describe("DisplayResourcesWorkspaceMainMenu", () => {
-  // 仕様上createボタン機能が存在しないためスキップ
-  describe.skip('As LU I can use the workspace create button', () => {
+  describe('As LU I can use the workspace create button', () => {
     it('As LU I can use the workspace create button if no folder is selected', async() => {
-      expect.assertions(2);
+      expect.assertions(1);
       const props = defaultProps(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
+      expect(page.displayMenu.exists()).toBeFalsy();
     });
 
     it('As LU I can use the workspace create button if I have the permission to create in the selected folder', async() => {
-      expect.assertions(2);
+      expect.assertions(1);
       const props = defaultPropsFolderOwned(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
+      expect(page.displayMenu.exists()).toBeFalsy();
     });
 
     it('As LU I cannot use the workspace create button if I do not have the permission to create in the selected folder', async() => {
-      expect.assertions(2);
+      expect.assertions(1);
       const props = defaultPropsFolderNotOwned(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeTruthy();
+      expect(page.displayMenu.exists()).toBeFalsy();
     });
 
     it('As LU I can see the workspace create button with password and totp disabled if metadataTypesSettings is not loaded', async() => {
-      expect.assertions(4);
+      expect.assertions(1);
       const props = defaultProps({metadataTypeSettings: null}); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
-      expect(page.displayMenu.newPasswordMenu.hasAttribute("disabled")).toBeTruthy();
-      expect(page.displayMenu.newTotpMenu.hasAttribute("disabled")).toBeTruthy();
+      expect(page.displayMenu.exists()).toBeFalsy();
     });
   });
 
-  // 仕様上createボタンとその内部機能が存在しないためスキップ
-  describe.skip('As LU I can create resource', () => {
+  describe('As LU I can create resource', () => {
     it('As LU I can create a resource if I have not selected any folder', async() => {
-      expect.assertions(5);
+      expect.assertions(3);
       const props = defaultProps(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
       const resourceTypeExpected = props.resourceTypes.getFirstBySlug(RESOURCE_TYPE_PASSWORD_AND_DESCRIPTION_SLUG);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
-      expect(page.displayMenu.newPasswordMenu).not.toBeNull();
-      await page.displayMenu.clickOnMenu(page.displayMenu.newPasswordMenu);
-      expect(props.dialogContext.open).toHaveBeenCalledWith(CreateResource, {folderParentId: null, resourceType: resourceTypeExpected});
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
+      expect(page.displayMenu.newPasswordMenu).toBeNull();
     });
 
     it('As LU I can create a resource v5', async() => {
-      expect.assertions(5);
+      expect.assertions(3);
       const props = defaultProps({metadataTypeSettings: new MetadataTypesSettingsEntity(defaultMetadataTypesSettingsV50FreshDto())}); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
       const resourceTypeExpected = props.resourceTypes.getFirstBySlug(RESOURCE_TYPE_V5_DEFAULT_SLUG);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
-      expect(page.displayMenu.newPasswordMenu).not.toBeNull();
-      await page.displayMenu.clickOnMenu(page.displayMenu.newPasswordMenu);
-      expect(props.dialogContext.open).toHaveBeenCalledWith(CreateResource, {folderParentId: null, resourceType: resourceTypeExpected});
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
+      expect(page.displayMenu.newPasswordMenu).toBeNull();
     });
 
     it('As LU I can create resource if I have selected a folder I am allowed to create in', async() => {
-      expect.assertions(5);
+      expect.assertions(3);
       const props = defaultPropsFolderOwned(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
       const resourceTypeExpected = props.resourceTypes.getFirstBySlug(RESOURCE_TYPE_PASSWORD_AND_DESCRIPTION_SLUG);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
-      expect(page.displayMenu.newPasswordMenu).not.toBeNull();
-      await page.displayMenu.clickOnMenu(page.displayMenu.newPasswordMenu);
-      expect(props.dialogContext.open).toHaveBeenCalledWith(CreateResource, {folderParentId: props.resourceWorkspaceContext.filter.payload.folder.id, resourceType: resourceTypeExpected});
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
+      expect(page.displayMenu.newPasswordMenu).toBeNull();
     });
 
     it('As LU I cannot create a resource with password if metadata type settings default is V4 and resource types is only v5', async() => {
-      expect.assertions(4);
+      expect.assertions(3);
       const props = defaultProps({resourceTypes: new ResourceTypesCollection(resourceTypesV5CollectionDto())}); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
       expect(page.displayMenu.newPasswordMenu).toBeNull();
     });
 
     it('As LU I cannot create a resource with password if metadata type settings default is V5 and resource types is only v4', async() => {
-      expect.assertions(4);
+      expect.assertions(3);
       const props = defaultProps({metadataTypeSettings: new MetadataTypesSettingsEntity(defaultMetadataTypesSettingsV50FreshDto()), resourceTypes: new ResourceTypesCollection(resourceTypesV4CollectionDto())}); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
       expect(page.displayMenu.newPasswordMenu).toBeNull();
     });
   });
 
-  // 仕様上createボタンとその内部機能が存在しないためスキップ
-  describe.skip('As LU I can create folder', () => {
+  describe('As LU I can create folder', () => {
     it('As LU I can create folder if I have not selected any folder', async() => {
-      expect.assertions(5);
+      expect.assertions(3);
       const props = defaultProps(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
-      expect(page.displayMenu.newFolderMenu).not.toBeNull();
-      await page.displayMenu.clickOnMenu(page.displayMenu.newFolderMenu);
-      expect(props.dialogContext.open).toHaveBeenCalledWith(CreateResourceFolder, {folderParentId: null});
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
+      expect(page.displayMenu.newPasswordMenu).toBeNull();
     });
 
     it('As LU I can create folder if I have selected a folder I am allowed to create in', async() => {
-      expect.assertions(5);
+      expect.assertions(3);
       const props = defaultPropsFolderOwned(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
-      expect(page.displayMenu.newFolderMenu).not.toBeNull();
-      await page.displayMenu.clickOnMenu(page.displayMenu.newFolderMenu);
-      expect(props.dialogContext.open).toHaveBeenCalledWith(CreateResourceFolder, {folderParentId: props.resourceWorkspaceContext.filter.payload.folder.id});
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
+      expect(page.displayMenu.newPasswordMenu).toBeNull();
     });
 
     it('As LU I cannot use the create folder button if disabled by API flag', async() => {
-      expect.assertions(4);
+      expect.assertions(3);
       const appContext = {
         siteSettings: {
           getServerTimezone: () => '',
@@ -198,107 +164,82 @@ describe("DisplayResourcesWorkspaceMainMenu", () => {
       const props = defaultProps({context}); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
       expect(page.displayMenu.newFolderMenu).toBeNull();
     });
 
     it('As LU I cannot use the create folder button if denied by RBAC', async() => {
-      expect.assertions(4);
+      expect.assertions(3);
       const props = propsWithDenyUiAction(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
       expect(page.displayMenu.newFolderMenu).toBeNull();
     });
   });
 
-  // 仕様上createボタンとその内部機能が存在しないためスキップ
-  describe.skip('As LU I can create standalone totp', () => {
+  describe('As LU I can create standalone totp', () => {
     it('As LU I can create a standalone totp if I have not selected any folder', async() => {
-      expect.assertions(5);
+      expect.assertions(3);
       const props = defaultProps(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
-      expect(page.displayMenu.newTotpMenu).not.toBeNull();
-      await page.displayMenu.clickOnMenu(page.displayMenu.newTotpMenu);
-      const resourceTypeExpected = props.resourceTypes.getFirstBySlug(RESOURCE_TYPE_TOTP_SLUG);
-      expect(props.workflowContext.start).toHaveBeenCalledWith(HandleTotpWorkflow, {mode: TotpWorkflowMode.CREATE_STANDALONE_TOTP, folderParentId: null, resourceType: resourceTypeExpected});
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
+      expect(page.displayMenu.newTotpMenu).toBeNull();
     });
 
     it('As LU I can create a standalone totp v5', async() => {
-      expect.assertions(5);
+      expect.assertions(3);
       const props = defaultProps({metadataTypeSettings: new MetadataTypesSettingsEntity(defaultMetadataTypesSettingsV50FreshDto())}); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
-      expect(page.displayMenu.newTotpMenu).not.toBeNull();
-      await page.displayMenu.clickOnMenu(page.displayMenu.newTotpMenu);
-      const resourceTypeExpected = props.resourceTypes.getFirstBySlug(RESOURCE_TYPE_V5_TOTP_SLUG);
-      expect(props.workflowContext.start).toHaveBeenCalledWith(HandleTotpWorkflow, {mode: TotpWorkflowMode.CREATE_STANDALONE_TOTP, folderParentId: null, resourceType: resourceTypeExpected});
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
+      expect(page.displayMenu.newTotpMenu).toBeNull();
     });
 
     it('As LU I can create standalone totp if I have selected a folder I am allowed to create in', async() => {
-      expect.assertions(5);
+      expect.assertions(3);
       const props = defaultPropsFolderOwned(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
-      expect(page.displayMenu.newTotpMenu).not.toBeNull();
-      await page.displayMenu.clickOnMenu(page.displayMenu.newTotpMenu);
-      const resourceTypeExpected = props.resourceTypes.getFirstBySlug(RESOURCE_TYPE_TOTP_SLUG);
-      expect(props.workflowContext.start).toHaveBeenCalledWith(HandleTotpWorkflow, {mode: TotpWorkflowMode.CREATE_STANDALONE_TOTP, folderParentId: props.resourceWorkspaceContext.filter.payload.folder.id, resourceType: resourceTypeExpected});
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
+      expect(page.displayMenu.newTotpMenu).toBeNull();
     });
 
     it('As LU I cannot create a standalone totp if metadata type settings default is V4 and resource types is only v5', async() => {
-      expect.assertions(4);
+      expect.assertions(3);
       const props = defaultProps({resourceTypes: new ResourceTypesCollection(resourceTypesV5CollectionDto())}); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
       expect(page.displayMenu.newTotpMenu).toBeNull();
     });
 
     it('As LU I cannot create a standalone totp if metadata type settings default is V5 and resource types is only v4', async() => {
-      expect.assertions(4);
+      expect.assertions(3);
       const props = defaultProps({metadataTypeSettings: new MetadataTypesSettingsEntity(defaultMetadataTypesSettingsV50FreshDto()), resourceTypes: new ResourceTypesCollection(resourceTypesV4CollectionDto())}); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.createMenu).not.toBeNull();
-      expect(page.displayMenu.hasCreateMenuDisabled()).toBeFalsy();
-      await page.displayMenu.clickOnMenu(page.displayMenu.createMenu);
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.createMenu).toBeNull();
       expect(page.displayMenu.newTotpMenu).toBeNull();
     });
   });
 
-  // 仕様上importボタン機能が存在しないためスキップ
-  describe.skip('As LU I can import resources', () => {
+  describe('As LU I can import resources', () => {
     it('As LU I can import resources', async() => {
       expect.assertions(2);
       const props = defaultProps(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
-      expect(page.displayMenu.importMenu).not.toBeNull();
+      expect(page.displayMenu.exists()).toBeFalsy();
+      expect(page.displayMenu.importMenu).toBeNull();
     });
 
     it('As LU I cannot use the workspace import button if disabled by API flag', async() => {
@@ -313,7 +254,7 @@ describe("DisplayResourcesWorkspaceMainMenu", () => {
       const props = defaultProps({context}); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
+      expect(page.displayMenu.exists()).toBeFalsy();
       expect(page.displayMenu.importMenu).toBeNull();
     });
 
@@ -322,7 +263,7 @@ describe("DisplayResourcesWorkspaceMainMenu", () => {
       const props = propsWithDenyUiAction(); // The props to pass
       const page = new DisplayResourcesWorkspaceMainMenuPage(props);
 
-      expect(page.displayMenu.exists()).toBeTruthy();
+      expect(page.displayMenu.exists()).toBeFalsy();
       expect(page.displayMenu.importMenu).toBeNull();
     });
   });
