63c2d16b58c319e2e2ed25437138cef562673473
Ryo ICHIKAWA <ryo-ichikawa@funaisoken.co.jp>
2025-03-17 17:35:45 +0900

    Skipする項目の粒度を修正しました。 「表示されない」ことそのもののテストはスキップしない 「非表示にした項目をクリックした際の振舞い」はスキップ

--- src/react-extension/components/Resource/DisplayResourcesList/DisplayResourcesListContextualMenu.test.js
+++ src/react-extension/components/Resource/DisplayResourcesList/DisplayResourcesListContextualMenu.test.js
@@ -23,7 +23,7 @@ import {
   propsResourceWithReadOnlyPermission,
   propsResourceWithUpdatePermission,
 } from "./DisplayResourcesListContextualMenu.test.data";
-import {ActionFeedbackContext} from "../../../contexts/ActionFeedbackContext";
+import { ActionFeedbackContext } from "../../../contexts/ActionFeedbackContext";
 import DeleteResource from "../DeleteResource/DeleteResource";
 import EditResource from "../EditResource/EditResource";
 import ShareDialog from "../../Share/ShareDialog";
@@ -33,9 +33,9 @@ import {
   plaintextSecretPasswordStringDto
 } from "../../../../shared/models/entity/plaintextSecret/plaintextSecretEntity.test.data";
 import HandleTotpWorkflow from "../HandleTotpWorkflow/HandleTotpWorkflow";
-import {TotpWorkflowMode} from "../HandleTotpWorkflow/HandleTotpWorkflowMode";
+import { TotpWorkflowMode } from "../HandleTotpWorkflow/HandleTotpWorkflowMode";
 import PasswordExpiryDialog from "../PasswordExpiryDialog/PasswordExpiryDialog";
-import {defaultPasswordExpirySettingsContext} from "../../../contexts/PasswordExpirySettingsContext.test.data";
+import { defaultPasswordExpirySettingsContext } from "../../../contexts/PasswordExpirySettingsContext.test.data";
 
 beforeEach(() => {
   jest.resetModules();
@@ -46,8 +46,8 @@ describe("DisplayResourcesListContextualMenu", () => {
 
   describe('As LU I should be able to access all the offered capabilities on resources I have owner access', () => {
     const props = defaultProps(); // The props to pass
-    jest.spyOn(ActionFeedbackContext._currentValue, 'displaySuccess').mockImplementation(() => {});
-    jest.spyOn(ActionFeedbackContext._currentValue, 'displayError').mockImplementation(() => {});
+    jest.spyOn(ActionFeedbackContext._currentValue, 'displaySuccess').mockImplementation(() => { });
+    jest.spyOn(ActionFeedbackContext._currentValue, 'displayError').mockImplementation(() => { });
 
     beforeEach(() => {
       page = new DisplayResourcesListContextualMenuPage(props);
@@ -76,7 +76,7 @@ describe("DisplayResourcesListContextualMenu", () => {
       expect(page.setExpiryDateItem).toBeNull();
     });
 
-    it('As LU I can start to copy the username of a resource', async() => {
+    it('As LU I can start to copy the username of a resource', async () => {
       expect.assertions(3);
       await page.copyUsername();
       expect(navigator.clipboard.writeText).toHaveBeenCalledWith(props.resource.metadata.username);
@@ -84,7 +84,7 @@ describe("DisplayResourcesListContextualMenu", () => {
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it.skip('As LU I can start to copy the password of a resource', async() => {
+    it.skip('As LU I can start to copy the password of a resource', async () => {
       expect.assertions(4);
       jest.spyOn(props.context.port, 'request').mockImplementationOnce(() => plaintextSecretPasswordStringDto());
       await page.copyPassword();
@@ -94,7 +94,7 @@ describe("DisplayResourcesListContextualMenu", () => {
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it('As LU I can start to copy the uri of a resource', async() => {
+    it('As LU I can start to copy the uri of a resource', async () => {
       expect.assertions(3);
       await page.copyUri();
       expect(navigator.clipboard.writeText).toHaveBeenCalledWith(props.resource.metadata.uris[0]);
@@ -102,7 +102,7 @@ describe("DisplayResourcesListContextualMenu", () => {
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it('As LU I can start to copy the permalink of a resource', async() => {
+    it('As LU I can start to copy the permalink of a resource', async () => {
       expect.assertions(3);
       await page.copyPermalink();
       expect(navigator.clipboard.writeText).toHaveBeenCalledWith(`${props.context.userSettings.getTrustedDomain()}/app/passwords/view/${props.resource.id}`);
@@ -110,58 +110,65 @@ describe("DisplayResourcesListContextualMenu", () => {
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it('As LU I can open a resource uri in new tab', async() => {
-      jest.spyOn(props.resourceWorkspaceContext, 'onGoToResourceUriRequested').mockImplementationOnce(() => {});
+    it('As LU I can open a resource uri in new tab', async () => {
+      jest.spyOn(props.resourceWorkspaceContext, 'onGoToResourceUriRequested').mockImplementationOnce(() => { });
       await page.openUri();
       expect(props.resourceWorkspaceContext.onGoToResourceUriRequested).toHaveBeenCalled();
     });
 
-    it.skip('As LU I can start to edit a resource', async() => {
+    // 編集メニューを非表示にしたためスキップ
+    it.skip('As LU I can start to edit a resource', async () => {
       await page.edit();
-      expect(props.dialogContext.open).toHaveBeenCalledWith(EditResource, {resource: props.resource});
+      expect(props.dialogContext.open).toHaveBeenCalledWith(EditResource, { resource: props.resource });
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it.skip('As LU I can start to share a resource', async() => {
+    // 共有メニューを非表示にしたためスキップ
+    it.skip('As LU I can start to share a resource', async () => {
       await page.share();
       expect(props.dialogContext.open).toHaveBeenCalledWith(ShareDialog);
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it.skip('As LU I can start to delete a resource', async() => {
+    // 削除メニューを非表示にしたためスキップ
+    it.skip('As LU I can start to delete a resource', async () => {
       await page.delete();
-      expect(props.dialogContext.open).toHaveBeenCalledWith(DeleteResource, {resources: [props.resource]});
+      expect(props.dialogContext.open).toHaveBeenCalledWith(DeleteResource, { resources: [props.resource] });
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it.skip('As LU I can start to mark a resource as expired', async() => {
+    // 無効化メニューを非表示にしたためスキップ
+    it.skip('As LU I can start to mark a resource as expired', async () => {
       expect.assertions(3);
       jest.spyOn(props.context.port, "request");
       await page.markAsExpired();
-      expect(props.context.port.request).toHaveBeenCalledWith("passbolt.resources.set-expiration-date", [{id: props.resource.id, expired: expect.any(String)}]);
+      expect(props.context.port.request).toHaveBeenCalledWith("passbolt.resources.set-expiration-date", [{ id: props.resource.id, expired: expect.any(String) }]);
       expect(ActionFeedbackContext._currentValue.displaySuccess).toHaveBeenCalled();
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it.skip('As LU I cannot start to mark a resource as expired if there is an error', async() => {
+    // 無効化メニューを非表示にしたためスキップ
+    it.skip('As LU I cannot start to mark a resource as expired if there is an error', async () => {
       expect.assertions(3);
       jest.spyOn(props.context.port, "request").mockImplementationOnce(() => { throw new Error('error'); });
       await page.markAsExpired();
-      expect(props.context.port.request).toHaveBeenCalledWith("passbolt.resources.set-expiration-date", [{id: props.resource.id, expired: expect.any(String)}]);
+      expect(props.context.port.request).toHaveBeenCalledWith("passbolt.resources.set-expiration-date", [{ id: props.resource.id, expired: expect.any(String) }]);
       expect(ActionFeedbackContext._currentValue.displayError).toHaveBeenCalled();
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it.skip('As LU I can mark a resource as expired', async() => {
+    // 無効化メニューを非表示にしたためスキップ
+    it.skip('As LU I can mark a resource as expired', async () => {
       expect.assertions(3);
       jest.spyOn(props.context.port, "request");
       await page.markAsExpired();
-      expect(props.context.port.request).toHaveBeenCalledWith("passbolt.resources.set-expiration-date", [{id: props.resource.id, expired: expect.any(String)}]);
+      expect(props.context.port.request).toHaveBeenCalledWith("passbolt.resources.set-expiration-date", [{ id: props.resource.id, expired: expect.any(String) }]);
       expect(ActionFeedbackContext._currentValue.displaySuccess).toHaveBeenCalled();
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it.skip('As LU I can start to set expiry expiry date of a resource', async() => {
+    // 無効化メニューを非表示にしたためスキップ
+    it.skip('As LU I can start to set expiry expiry date of a resource', async () => {
       await page.setExpiryDate();
       expect(props.dialogContext.open).toHaveBeenCalledWith(PasswordExpiryDialog, {
         resources: [props.resource]
@@ -170,10 +177,9 @@ describe("DisplayResourcesListContextualMenu", () => {
     });
   });
 
-  // 仕様上totpでのログイン機能は存在しないためテストをスキップ
-  describe.skip('As LU I should be able to access all the offered capabilities on totp resources I have owner access', () => {
+  describe('As LU I should be able to access all the offered capabilities on totp resources I have owner access', () => {
     const props = propsResourceTotp(); // The props to pass
-    jest.spyOn(ActionFeedbackContext._currentValue, 'displaySuccess').mockImplementationOnce(() => {});
+    jest.spyOn(ActionFeedbackContext._currentValue, 'displaySuccess').mockImplementationOnce(() => { });
 
     beforeEach(() => {
       page = new DisplayResourcesListContextualMenuPage(props);
@@ -186,29 +192,23 @@ describe("DisplayResourcesListContextualMenu", () => {
     it('As LU I should see all menu name', () => {
       expect(page.copyUsernameItem).not.toBeNull();
       expect(page.copyUsernameItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.copyPasswordItem).not.toBeNull();
-      expect(page.copyPasswordItem.hasAttribute("disabled")).toBeFalsy();
+      expect(page.copyPasswordItem).toBeNull();
       expect(page.copyUriItem).not.toBeNull();
       expect(page.copyUriItem.hasAttribute("disabled")).toBeFalsy();
       expect(page.copyPermalinkItem).not.toBeNull();
       expect(page.copyPermalinkItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.copyTotpItem).not.toBeNull();
-      expect(page.copyTotpItem.hasAttribute("disabled")).toBeFalsy();
+      expect(page.copyTotpItem).toBeNull();
       expect(page.openUriItem).not.toBeNull();
       expect(page.openUriItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.editItem).not.toBeNull();
-      expect(page.editItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.shareItem).not.toBeNull();
-      expect(page.shareItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.deleteItem).not.toBeNull();
-      expect(page.deleteItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.markAsExpiredItem).not.toBeNull();
-      expect(page.markAsExpiredItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.setExpiryDateItem).not.toBeNull();
-      expect(page.setExpiryDateItem.hasAttribute("disabled")).toBeFalsy();
+      expect(page.editItem).toBeNull();
+      expect(page.shareItem).toBeNull();
+      expect(page.deleteItem).toBeNull();
+      expect(page.markAsExpiredItem).toBeNull();
+      expect(page.setExpiryDateItem).toBeNull();
     });
 
-    it('As LU I can start to copy the totp of a resource', async() => {
+    // コピー機能を非表示にしたためスキップ
+    it.skip('As LU I can start to copy the totp of a resource', async () => {
       expect.assertions(4);
       jest.spyOn(props.context.port, 'request').mockImplementationOnce(() => plaintextSecretPasswordDescriptionTotpDto());
       await page.copyTotp();
@@ -219,10 +219,9 @@ describe("DisplayResourcesListContextualMenu", () => {
     });
   });
 
-  // 仕様上totpでのログイン機能は存在しないためテストをスキップ
-  describe.skip('As LU I should be able to access all the offered capabilities on a standalone totp resources I have owner access', () => {
+  describe('As LU I should be able to access all the offered capabilities on a standalone totp resources I have owner access', () => {
     const props = propsResourceStandaloneTotp(); // The props to pass
-    jest.spyOn(ActionFeedbackContext._currentValue, 'displaySuccess').mockImplementationOnce(() => {});
+    jest.spyOn(ActionFeedbackContext._currentValue, 'displaySuccess').mockImplementationOnce(() => { });
 
     beforeEach(() => {
       page = new DisplayResourcesListContextualMenuPage(props);
@@ -235,25 +234,21 @@ describe("DisplayResourcesListContextualMenu", () => {
     it('As LU I should see all menu name', () => {
       expect(page.copyUsernameItem).not.toBeNull();
       expect(page.copyUsernameItem.hasAttribute("disabled")).toBeTruthy();
-      expect(page.copyPasswordItem).not.toBeNull();
-      expect(page.copyPasswordItem.hasAttribute("disabled")).toBeTruthy();
+      expect(page.copyPasswordItem).toBeNull();
       expect(page.copyUriItem).not.toBeNull();
       expect(page.copyUriItem.hasAttribute("disabled")).toBeFalsy();
       expect(page.copyPermalinkItem).not.toBeNull();
       expect(page.copyPermalinkItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.copyTotpItem).not.toBeNull();
-      expect(page.copyTotpItem.hasAttribute("disabled")).toBeFalsy();
+      expect(page.copyTotpItem).toBeNull();
       expect(page.openUriItem).not.toBeNull();
       expect(page.openUriItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.editItem).not.toBeNull();
-      expect(page.editItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.shareItem).not.toBeNull();
-      expect(page.shareItem.hasAttribute("disabled")).toBeFalsy();
-      expect(page.deleteItem).not.toBeNull();
-      expect(page.deleteItem.hasAttribute("disabled")).toBeFalsy();
+      expect(page.editItem).toBeNull();
+      expect(page.shareItem).toBeNull();
+      expect(page.deleteItem).toBeNull();
     });
 
-    it('As LU I can start to copy the totp of a resource', async() => {
+    // コピー機能を非表示にしたためスキップ
+    it.skip('As LU I can start to copy the totp of a resource', async () => {
       expect.assertions(4);
       jest.spyOn(props.context.port, 'request').mockImplementationOnce(() => plaintextSecretPasswordDescriptionTotpDto());
       await page.copyTotp();
@@ -263,10 +258,11 @@ describe("DisplayResourcesListContextualMenu", () => {
       expect(props.hide).toHaveBeenCalled();
     });
 
-    it('As LU I can start to edit the totp of a resource', async() => {
+    // 編集機能を非表示にしたためスキップ
+    it.skip('As LU I can start to edit the totp of a resource', async () => {
       expect.assertions(2);
       await page.edit();
-      expect(props.workflowContext.start).toHaveBeenCalledWith(HandleTotpWorkflow, {mode: TotpWorkflowMode.EDIT_STANDALONE_TOTP});
+      expect(props.workflowContext.start).toHaveBeenCalledWith(HandleTotpWorkflow, { mode: TotpWorkflowMode.EDIT_STANDALONE_TOTP });
       expect(props.hide).toHaveBeenCalled();
     });
   });
@@ -278,7 +274,7 @@ describe("DisplayResourcesListContextualMenu", () => {
       page = new DisplayResourcesListContextualMenuPage(props);
     });
 
-    it('As LU I should not be able to edit/share/delete/expire a password I have read only access', async() => {
+    it('As LU I should not be able to edit/share/delete/expire a password I have read only access', async () => {
       expect.assertions(14);
       expect(page.copyUsernameItem).not.toBeNull();
       expect(page.copyUsernameItem.hasAttribute("disabled")).toBeFalsy();
@@ -304,7 +300,7 @@ describe("DisplayResourcesListContextualMenu", () => {
       page = new DisplayResourcesListContextualMenuPage(props);
     });
 
-    it('As LU I should not be able to share a password I have update access', async() => {
+    it('As LU I should not be able to share a password I have update access', async () => {
       expect(page.copyUsernameItem).not.toBeNull();
       expect(page.copyUsernameItem.hasAttribute('disabled')).toBeFalsy();
       expect(page.copyPasswordItem).toBeNull();
@@ -329,7 +325,7 @@ describe("DisplayResourcesListContextualMenu", () => {
       page = new DisplayResourcesListContextualMenuPage(props);
     });
 
-    it('As LU I should not see the copy password to clipboard if denied by rbac', async() => {
+    it('As LU I should not see the copy password to clipboard if denied by rbac', async () => {
       expect(page.copyUsernameItem).not.toBeNull();
       expect(page.copyUsernameItem.hasAttribute('disabled')).toBeFalsy();
       expect(page.copyPasswordItem).toBeNull();
