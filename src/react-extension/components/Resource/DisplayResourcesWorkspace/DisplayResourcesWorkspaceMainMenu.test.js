4a57a4306e963284ccb8543f2d7718b15104c892
masahiro-takusagawa-funai <masahiro-takusagawa@funaisoken.co.jp>
2025-03-17 16:47:59 +0900

    Revert "Passbolt画面create・importボタン削除対応UT修正"
    This reverts commit 123250ad9c7362079ce3fbde615c62e5f78abf5f.

--- src/react-extension/components/Resource/DisplayResourcesWorkspace/DisplayResourcesWorkspaceMainMenu.test.js
+++ src/react-extension/components/Resource/DisplayResourcesWorkspace/DisplayResourcesWorkspaceMainMenu.test.js
@@ -42,8 +42,7 @@ beforeEach(() => {
 });
 
 describe("DisplayResourcesWorkspaceMainMenu", () => {
-  // 仕様上createボタン機能が存在しないためスキップ
-  describe.skip('As LU I can use the workspace create button', () => {
+  describe('As LU I can use the workspace create button', () => {
     it('As LU I can use the workspace create button if no folder is selected', async() => {
       expect.assertions(2);
       const props = defaultProps(); // The props to pass
@@ -84,7 +83,6 @@ describe("DisplayResourcesWorkspaceMainMenu", () => {
     });
   });
 
-  // 仕様上createボタンとその内部機能が存在しないためスキップ
   describe('As LU I can create resource', () => {
     it('As LU I can create a resource if I have not selected any folder', async() => {
       expect.assertions(5);
@@ -156,7 +154,6 @@ describe("DisplayResourcesWorkspaceMainMenu", () => {
     });
   });
 
-  // 仕様上createボタンとその内部機能が存在しないためスキップ
   describe('As LU I can create folder', () => {
     it('As LU I can create folder if I have not selected any folder', async() => {
       expect.assertions(5);
@@ -218,7 +215,6 @@ describe("DisplayResourcesWorkspaceMainMenu", () => {
     });
   });
 
-  // 仕様上createボタンとその内部機能が存在しないためスキップ
   describe('As LU I can create standalone totp', () => {
     it('As LU I can create a standalone totp if I have not selected any folder', async() => {
       expect.assertions(5);
@@ -290,8 +286,7 @@ describe("DisplayResourcesWorkspaceMainMenu", () => {
     });
   });
 
-  // 仕様上importボタン機能が存在しないためスキップ
-  describe.skip('As LU I can import resources', () => {
+  describe('As LU I can import resources', () => {
     it('As LU I can import resources', async() => {
       expect.assertions(2);
       const props = defaultProps(); // The props to pass
