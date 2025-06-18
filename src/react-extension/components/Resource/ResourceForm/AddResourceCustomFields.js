a9b71d99db512339192de3c965c1319448193b5c
Benjamin Monnot <benjamin@passbolt.com>
2025-06-18 17:08:00 +0200

    PB-43284 CUF - WP3.2 Create the CustomFieldForm for the create/edit dialog

---
+++ src/react-extension/components/Resource/ResourceForm/AddResourceCustomFields.js
@@ -0,0 +1,180 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         5.3.0
+ */
+
+import PropTypes from "prop-types";
+import React, {Component} from "react";
+import {Trans, withTranslation} from "react-i18next";
+import DeleteSVG from "../../../../img/svg/delete.svg";
+import AddSVG from "../../../../img/svg/add.svg";
+import SecureTextarea from "../../../../shared/components/SecureTextarea/SecureTextarea";
+import CustomFieldEntity from "../../../../shared/models/entity/customField/customFieldEntity";
+import Tooltip from "../../Common/Tooltip/Tooltip";
+
+class AddResourceCustomFields extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.defaultState;
+    this.bindCallbacks();
+  }
+
+  get defaultState() {
+    return {
+      totalCharacters: 0,
+    };
+  }
+
+  /**
+   * Bind callbacks methods
+   * @return {void}
+   */
+  bindCallbacks() {
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleAddCustomFieldsClick = this.handleAddCustomFieldsClick.bind(this);
+    this.handleAddCustomFieldsClick = this.handleAddCustomFieldsClick.bind(this);
+  }
+
+  /**
+   * Get the translation function
+   * @returns {function(...[*]=)}
+   */
+  get translate() {
+    return this.props.t;
+  }
+
+  /**
+   * Handle form input change.
+   * @params {ReactEvent} The react event.
+   */
+  handleInputChange(event) {
+    if (this.props.onChange) {
+      this.props.onChange(event);
+    }
+  }
+
+  /**
+   * Handle the click on the add custom fields button
+   */
+  handleAddCustomFieldsClick() {
+    const eventCustomField = {
+      target: {
+        name: `secret.custom_fields.${this.props.resource.secret.custom_fields.length}`,
+        value: CustomFieldEntity.createFromDefault()
+      }
+    };
+    this.props.onChange?.(eventCustomField);
+  }
+
+  /**
+   * Handle the click on the delete custom fields button
+   * @param {number} index The index of the uri to delete
+   */
+  handleDeleteCustomFieldClick(index) {
+    const eventCustomField = {
+      target: {
+        name: `secret.custom_fields.${index}`,
+        value: null
+      }
+    };
+    this.props.onChange?.(eventCustomField);
+  }
+
+  /**
+   * Can add custom field
+   * @returns {boolean}
+   */
+  get canAddCustomField() {
+    return this.props.resource?.secret?.custom_fields?.length < 33;
+  }
+
+  /**
+   * Display the content surrounded or not with a tooltip.
+   * @param {ReactDOM} content content to display
+   * @param {boolean} isDisabled if disabled the tooltip is added
+   * @returns {ReactDOM}
+   */
+  addTooltipOnDisabledElement(content, isDisabled) {
+    return isDisabled
+      ? <Tooltip message={<Trans>You have reach the row limit.</Trans>} direction="bottom">{content}</Tooltip>
+      : <>{content}</>;
+  }
+
+  /*
+   * =============================================================
+   *  Render view
+   * =============================================================
+   */
+  render() {
+    return (
+      <>
+        <div className="title">
+          <h2><Trans>Custom fields</Trans></h2>
+        </div>
+        <div className="header">
+          <div className="key">
+            <span className="label"><Trans>Key</Trans></span>
+            <span className="subinfo"><Trans>Searchable Metadata</Trans></span>
+          </div>
+          <div className="divider-wrapper">
+            <span className="divider"></span>
+          </div>
+          <div className="value">
+            <span className="label"><Trans>Value</Trans></span>
+            <span className="subinfo"><Trans>Non-Searchable Secret</Trans></span>
+          </div>
+        </div>
+        <div className="content">
+          <div className="custom-fields-fields">
+            {this.props.resource?.secret.custom_fields.map((custom_field, index) => (
+              <div key={index} className="custom-field-row">
+                <div className="input custom-field">
+                  <input id={`resource-custom-fields-key-${index}`} disabled={this.props.disabled} name={`secret.custom_fields.${index}.metadata_key`} maxLength="255" type="text" autoComplete="off" placeholder={this.translate("Key")} value={custom_field.metadata_key} onChange={this.handleInputChange}/>
+                  <SecureTextarea
+                    id={`resource-custom-fields-value-${index}`}
+                    name={`secret.custom_fields.${index}.secret_value`}
+                    placeholder={this.translate("Value")}
+                    value={custom_field.secret_value}
+                    onChange={this.handleInputChange}
+                    disabled={this.props.disabled}
+                  />
+                  <button type="button" className="button-transparent inline" id={`resource-delete-custom-field-${index}`} onClick={() => this.handleDeleteCustomFieldClick(index)}><DeleteSVG/></button>
+                </div>
+              </div>
+            ))
+            }
+            <div className="custom-field-add">
+              {this.addTooltipOnDisabledElement(
+                <button type="button" disabled={!this.canAddCustomField} onClick={this.handleAddCustomFieldsClick}>
+                  <AddSVG/>
+                  <span><Trans>Add Row</Trans></span>
+                </button>,
+                !this.canAddCustomField
+              )}
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+AddResourceCustomFields.propTypes = {
+  resource: PropTypes.object, // The resource to edit or create
+  onChange: PropTypes.func, //The resource setter
+  t: PropTypes.func, // The translation function
+  warnings: PropTypes.object, //The warnings validation
+  disabled: PropTypes.bool // The disabled property
+};
+
+export default  withTranslation('common')(AddResourceCustomFields);
+
---
+++ src/react-extension/components/Resource/ResourceForm/AddResourceCustomFields.test.js
@@ -0,0 +1,129 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         5.3.0
+ */
+
+/**
+ * Unit tests on AddResourceCustomFields in regard of specifications
+ */
+
+import {defaultProps} from './AddResourceCustomFields.test.data';
+import AddResourceCustomFieldsPage from './AddResourceCustomFields.test.page';
+import {defaultResourceFormDto} from "../../../../shared/models/entity/resource/resourceFormEntity.test.data";
+import {
+  minimalSecretDataV5StandaloneCustomFieldsCollectionDtos
+} from "../../../../shared/models/entity/secretData/secretDataV5StandaloneCustomFieldsCollection.test.data";
+import CustomFieldEntity from "../../../../shared/models/entity/customField/customFieldEntity";
+
+beforeEach(() => {
+  jest.resetModules();
+});
+
+describe("AddResourceCustomFields", () => {
+  let page; // The page to test against
+
+  describe('As LU I can see the custom fields form.', () => {
+    it('As LU I can see the custom fields form.', () => {
+      expect.assertions(7);
+
+      const props = defaultProps();
+      page = new AddResourceCustomFieldsPage(props);
+
+      expect(page.exists).toBeTruthy();
+      expect(page.title.textContent).toEqual("Custom fields");
+      expect(page.getCustomFieldKey(0).value).toEqual("Key 0");
+      expect(page.getCustomFieldValue(0).value).toEqual("Value 0");
+      expect(page.getCustomFieldKey(1).value).toEqual("Key 1");
+      expect(page.getCustomFieldValue(1).value).toEqual("Value 1");
+      expect(page.addCustomField.hasAttribute("disabled")).toBeFalsy();
+    });
+
+    it('As LU I can see the custom fields form empty.', () => {
+      expect.assertions(5);
+
+      const secret = minimalSecretDataV5StandaloneCustomFieldsCollectionDtos();
+      secret.custom_fields[0] = CustomFieldEntity.createFromDefault();
+      const props = defaultProps({
+        resource: defaultResourceFormDto({
+          secret: secret
+        })
+      });
+      page = new AddResourceCustomFieldsPage(props);
+
+      expect(page.exists).toBeTruthy();
+      expect(page.title.textContent).toEqual("Custom fields");
+      expect(page.getCustomFieldKey(0).value).toEqual("");
+      expect(page.getCustomFieldValue(0).value).toEqual("");
+      expect(page.addCustomField.hasAttribute("disabled")).toBeFalsy();
+    });
+  });
+
+  describe('Fill form custom fields', () => {
+    it('Enter custom field key and value should call callback function.', async() => {
+      expect.assertions(5);
+
+      let name, value;
+      const props = defaultProps();
+      jest.spyOn(props, "onChange").mockImplementation(event => {
+        name = event.target.name;
+        value = event.target.value;
+      });
+      page = new AddResourceCustomFieldsPage(props);
+      await page.fillInput(page.getCustomFieldKey(0), "Key Updated");
+      expect(name).toEqual("secret.custom_fields.0.metadata_key");
+      expect(value).toEqual("Key Updated");
+
+      await page.fillInput(page.getCustomFieldValue(1), "Value Updated");
+      expect(props.onChange).toHaveBeenCalledTimes(2);
+      expect(name).toEqual("secret.custom_fields.1.secret_value");
+      expect(value).toEqual("Value Updated");
+    });
+  });
+
+  describe('As LU I should see add custom field disable if there is 32 custom fields.', () => {
+    it('As LU I can not add custom field when I reach the limit.', async() => {
+      expect.assertions(1);
+      const secret = minimalSecretDataV5StandaloneCustomFieldsCollectionDtos();
+      for (let i = 0; i < 33; i++) {
+        secret.custom_fields[i] = CustomFieldEntity.createFromDefault();
+      }
+      const props = defaultProps({
+        resource: defaultResourceFormDto({
+          secret: secret
+        })
+      });
+      page = new AddResourceCustomFieldsPage(props);
+
+      expect(page.addCustomField.hasAttribute("disabled")).toBeTruthy();
+    });
+  });
+
+  describe('As LU I should delete a custom field.', () => {
+    it('As LU I can see the custom field form deleted.', async() => {
+      expect.assertions(3);
+
+      let name, value;
+      const props = defaultProps();
+      jest.spyOn(props, "onChange").mockImplementation(event => {
+        name = event.target.name;
+        value = event.target.value;
+      });
+      page = new AddResourceCustomFieldsPage(props);
+
+      await page.click(page.getDeleteCustomField(0));
+
+      expect(props.onChange).toHaveBeenCalledTimes(1);
+      expect(name).toEqual("secret.custom_fields.0");
+      expect(value).toEqual(null);
+    });
+  });
+});
