dcf53172452fa8ffca85ba753dc74bee45280c96
Stéphane Loegel <stephane@passbolt.com>
2025-08-05 15:31:42 +0200

    PB-44403 - EIGS - WP4.4 Update DisplayContentTypesMetadataKeyAdministration to take into account getting started changes

--- src/react-extension/components/Administration/DisplayContentTypesEncryptedMetadataAdministration/DisplayContentTypesEncryptedMetadataAdministration.js
+++ src/react-extension/components/Administration/DisplayContentTypesEncryptedMetadataAdministration/DisplayContentTypesEncryptedMetadataAdministration.js
@@ -223,6 +223,9 @@ class DisplayContentTypesEncryptedMetadataAdministration extends Component {
     const errors = this.state.hasAlreadyBeenValidated ? this.validateForm(this.state.settings) : null;
     const warnings = this.verifyDataHealth(this.state.settings, this.props.resourceTypes, this.metadataKeys);
     const hasSettingsChanges = this.hasSettingsChanges(this.originalSettings, this.formSettings, this.state.settings);
+    const isFeatureBeta = this.props.context.siteSettings.isFeatureBeta("metadata");
+
+    const shouldDisplayAWarningBlock = isFeatureBeta || hasSettingsChanges;
 
     return (
       <div className="row">
@@ -373,13 +376,18 @@ class DisplayContentTypesEncryptedMetadataAdministration extends Component {
               </div>
             </form>
           </div>
-          {hasSettingsChanges &&
-            <div className="warning message form-banner">
-              <div>
-                <p>
-                  <Trans>Don&apos;t forget to save your settings to apply your modification.</Trans>
-                </p>
-              </div>
+          {shouldDisplayAWarningBlock &&
+            <div className="warning message">
+              {isFeatureBeta &&
+                <div>
+                  <b><Trans>Warning:</Trans></b> <Trans>Your current API version includes beta support for encrypted metadata and new resource types.</Trans> <Trans>To ensure stability and avoid potential issues, upgrade to the latest version before enabling these features.</Trans>
+                </div>
+              }
+              {hasSettingsChanges &&
+                <div className="form-banner">
+                  <p><b><Trans>Warning:</Trans></b> <Trans>Don&apos;t forget to save your settings to apply your modification.</Trans></p>
+                </div>
+              }
             </div>
           }
         </div>
