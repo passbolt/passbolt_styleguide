f5904dc5cd8978a9d8fe30d05a6f15169e56cda8
Sowmya <sowmya@passbolt.com>
2026-05-07 16:18:07 +0200

    PB-51227 OFM - WP1.1 Create OfflineSettingsEntity

---
+++ src/shared/models/entity/offline/offlineSettingsEntity.test.data.js
@@ -0,0 +1,35 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         5.13.0
+ */
+
+import { v4 as uuid } from "uuid";
+
+export const defaultOfflineSettingsDto = (data = {}) => {
+  const defaultData = {
+    session_duration: 3600,
+    maximum_retention_period: 86400,
+  };
+  return Object.assign(defaultData, data);
+};
+
+export const defaultOfflineSettingsDtoFromApi = (data = {}) => {
+  const id = data.id || uuid();
+  const defaultData = defaultOfflineSettingsDto({
+    id,
+    created: "2025-08-06T10:05:46+00:00",
+    created_by: uuid(),
+    modified: "2025-08-06T10:05:46+00:00",
+    modified_by: uuid(),
+  });
+  return Object.assign(defaultData, data);
+};
