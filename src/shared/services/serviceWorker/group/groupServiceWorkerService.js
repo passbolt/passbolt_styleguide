238212caea93ff87e46e3f85f7a077c49e33b366
Cédric HERZOG <cedric.herzog@passbolt.com>
2026-05-26 07:55:28 +0000

    PB-51791 - PCD 2.10 - Add new methods getByIds, getGroupsUsersByGropuId on GroupServiceWorkerService
    Signed-off-by: Cédric HERZOG <cedric.herzog@passbolt.com>

--- src/shared/services/serviceWorker/group/groupServiceWorkerService.js
+++ src/shared/services/serviceWorker/group/groupServiceWorkerService.js
@@ -11,8 +11,11 @@
  * @link          https://www.passbolt.com Passbolt(tm)
  * @since         5.7.0
  */
+import GroupsUsersCollection from "../../../models/entity/groupUser/groupsUsersCollection";
 
 export const GROUPS_FIND_MY_GROUPS = "passbolt.groups.find-my-groups";
+export const GROUPS_GET_BY_IDS = "passbolt.groups.get-by-ids";
+export const GROUPS_USERS_GET_BY_GROUP_ID = "passbolt.groups_users.get-by-group-id";
 
 export default class GroupServiceWorkerService {
   /**
@@ -29,5 +32,29 @@ export default class GroupServiceWorkerService {
    */
   async findMyGroups() {
     return await this.port.request(GROUPS_FIND_MY_GROUPS);
+  }
+
+  /**
+   * Get the groups matching the given ids. The list comes from the service-worker local-storage cache
+   * when initialised, otherwise it is fetched from the API.
+   * Returns the raw DTO array; the styleguide does not yet host `GroupsCollection` (it lives in the
+   * browser extension). A follow-up ticket will move that entity to the styleguide and tighten the
+   * return type to `Promise<GroupsCollection>`.
+   * @param {Array<string>} groupIds The ids of the groups to retrieve.
+   * @returns {Promise<Array<object>>}
+   */
+  async getByIds(groupIds) {
+    return await this.port.request(GROUPS_GET_BY_IDS, groupIds);
+  }
+
+  /**
+   * Get the members of the group matching the given id. The list comes from the service-worker
+   * local-storage cache when initialised, otherwise it is fetched from the API.
+   * @param {string} groupId The id of the group whose members are requested.
+   * @returns {Promise<GroupsUsersCollection>}
+   */
+  async getGroupsUsersByGroupId(groupId) {
+    const groupsUsersDto = await this.port.request(GROUPS_USERS_GET_BY_GROUP_ID, groupId);
+    return new GroupsUsersCollection(groupsUsersDto);
   }
 }
