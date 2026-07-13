a65d8e8f64e438b48d1527ae7ff24704dd53d00c
Stéphane Loegel <stephane@passbolt.com>
2026-07-13 15:35:03 +0200

    PB-53097: fix edit and create resource dialog closing at the wrong time

--- src/react-extension/components/Resource/CreateResource/CreateResource.js
+++ src/react-extension/components/Resource/CreateResource/CreateResource.js
@@ -86,6 +86,7 @@ class CreateResource extends Component {
     this.rejectCreationConfirmation = this.rejectCreationConfirmation.bind(this);
     this.consumePasswordEntropyError = this.consumePasswordEntropyError.bind(this);
     this.save = this.save.bind(this);
+    this.props.setFocusBackListener(this.handleFocusBack.bind(this));
   }
 
   /**
@@ -410,7 +411,6 @@ class CreateResource extends Component {
    */
   async save(resource) {
     await this.props.onSubmit(resource);
-    this.handleClose();
   }
 
   /*
@@ -499,6 +499,15 @@ class CreateResource extends Component {
       resource: this.resourceFormEntity.toDto(),
       resourceFormSelected: ResourceEditCreateFormEnumerationTypes.NOTE,
       resourceType,
+    });
+  }
+
+  /**
+   * Triggered when the dialog should have the focus back.
+   */
+  handleFocusBack() {
+    this.setState({
+      isProcessing: false,
     });
   }
 
@@ -612,6 +621,7 @@ CreateResource.propTypes = {
   passwordPoliciesContext: PropTypes.object, // The password policy context
   resourceTypes: PropTypes.instanceOf(ResourceTypesCollection), // The resource types collection
   resourceType: PropTypes.instanceOf(ResourceTypeEntity).isRequired, // The resource types entity
+  setFocusBackListener: PropTypes.func, // Event handler for handling focusing back on this dialog
   t: PropTypes.func, // The translation function
 };
 
